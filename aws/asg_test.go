56101fa0b73e774661001b484b06eeaa8c284dc6
Oluwatoni Solarin-Sodara <toni.edward@outlook.com>
2018-01-29 16:36:44 +0100

    Add tests for Auto Scaling Group nuking functionality

---
+++ aws/asg_test.go
@@ -0,0 +1,112 @@
+package aws
+
+import (
+	"testing"
+
+	awsgo "github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/autoscaling"
+	"github.com/aws/aws-sdk-go/service/ec2"
+	"github.com/gruntwork-io/gruntwork-cli/errors"
+	"github.com/stretchr/testify/assert"
+)
+
+func createTestAutoScalingGroup(t *testing.T, session *session.Session, name string) {
+	svc := autoscaling.New(session)
+	instance := createTestEC2Instance(t, session, name)
+
+	// EC2 Instance must be in a running state before ASG can be created
+	err := ec2.New(session).WaitUntilInstanceRunning(&ec2.DescribeInstancesInput{
+		Filters: []*ec2.Filter{
+			&ec2.Filter{
+				Name:   awsgo.String("instance-id"),
+				Values: []*string{instance.InstanceId},
+			},
+		},
+	})
+
+	if err != nil {
+		assert.Fail(t, errors.WithStackTrace(err).Error())
+	}
+
+	param := &autoscaling.CreateAutoScalingGroupInput{
+		AutoScalingGroupName: &name,
+		InstanceId:           instance.InstanceId,
+		MinSize:              awsgo.Int64(1),
+		MaxSize:              awsgo.Int64(2),
+	}
+
+	_, err = svc.CreateAutoScalingGroup(param)
+	if err != nil {
+		assert.Failf(t, "Could not create test ASG: %s", errors.WithStackTrace(err).Error())
+	}
+
+	err = svc.WaitUntilGroupExists(&autoscaling.DescribeAutoScalingGroupsInput{
+		AutoScalingGroupNames: []*string{&name},
+	})
+
+	if err != nil {
+		assert.Fail(t, errors.WithStackTrace(err).Error())
+	}
+}
+
+func TestListAutoScalingGroups(t *testing.T) {
+	session, err := session.NewSession(&awsgo.Config{
+		Region: awsgo.String("us-west-2")},
+	)
+
+	if err != nil {
+		assert.Fail(t, errors.WithStackTrace(err).Error())
+	}
+
+	groupName := "aws-nuke-test-" + uniqueID()
+	createTestAutoScalingGroup(t, session, groupName)
+
+	groupNames, err := getAllAutoScalingGroups(session, "us-west-2")
+	if err != nil {
+		assert.Fail(t, "Unable to fetch list of Auto Scaling Groups")
+	}
+
+	assert.Contains(t, awsgo.StringValueSlice(groupNames), groupName)
+}
+
+func TestNukeAutoScalingGroups(t *testing.T) {
+	session, err := session.NewSession(&awsgo.Config{
+		Region: awsgo.String("us-west-2")},
+	)
+	svc := autoscaling.New(session)
+
+	if err != nil {
+		assert.Fail(t, errors.WithStackTrace(err).Error())
+	}
+
+	groupName := "aws-nuke-test-" + uniqueID()
+	createTestAutoScalingGroup(t, session, groupName)
+
+	_, err = svc.DescribeAutoScalingGroups(&autoscaling.DescribeAutoScalingGroupsInput{
+		AutoScalingGroupNames: []*string{&groupName},
+	})
+
+	if err != nil {
+		assert.Fail(t, errors.WithStackTrace(err).Error())
+	}
+
+	if err := nukeAllAutoScalingGroups(session, []*string{&groupName}); err != nil {
+		assert.Fail(t, errors.WithStackTrace(err).Error())
+	}
+
+	err = svc.WaitUntilGroupNotExists(&autoscaling.DescribeAutoScalingGroupsInput{
+		AutoScalingGroupNames: []*string{&groupName},
+	})
+
+	if err != nil {
+		assert.Fail(t, errors.WithStackTrace(err).Error())
+	}
+
+	groupNames, err := getAllAutoScalingGroups(session, "us-west-2")
+	if err != nil {
+		assert.Fail(t, "Unable to fetch list of Auto Scaling Groups")
+	}
+
+	assert.NotContains(t, awsgo.StringValueSlice(groupNames), groupName)
+}
