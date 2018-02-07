f18e711d5016b56e0ab6b1da1d27980d36469ce6
Oluwatoni Solarin-Sodara <toni.edward@outlook.com>
2018-02-07 12:19:44 +0100

    wait until elb is deleted before returning from nuke function

--- aws/elb.go
+++ aws/elb.go
@@ -1,11 +1,27 @@
 package aws
 
 import (
+	"time"
+
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/elb"
 	"github.com/gruntwork-io/aws-nuke/logging"
 	"github.com/gruntwork-io/gruntwork-cli/errors"
 )
+
+func waitUntilElbDeleted(svc *elb.ELB, input *elb.DescribeLoadBalancersInput) error {
+	for i := 0; i < 30; i++ {
+		_, err := svc.DescribeLoadBalancers(input)
+		if err != nil {
+			// an error is returned when ELB no longer exists
+			return nil
+		}
+
+		time.Sleep(1 * time.Second)
+	}
+
+	panic("ELBs failed to delete")
+}
 
 // Returns a formatted string of ELB names
 func getAllElbInstances(session *session.Session, region string) ([]*string, error) {
@@ -46,6 +62,14 @@ func nukeAllElbInstances(session *session.Session, names []*string) error {
 		}
 
 		logging.Logger.Infof("Deleted ELB: %s", *name)
+	}
+
+	err := waitUntilElbDeleted(svc, &elb.DescribeLoadBalancersInput{
+		LoadBalancerNames: names,
+	})
+
+	if err != nil {
+		return errors.WithStackTrace(err)
 	}
 
 	logging.Logger.Infof("[OK] %d Elastic Load Balancer(s) deleted in %s", len(names), *session.Config.Region)
