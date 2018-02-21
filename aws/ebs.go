94cd8ae8328f810dd314109ceb72419a754a3f30
Oluwatoni Solarin-Sodara <toni.edward@outlook.com>
2018-02-21 15:00:39 +0100

    log ebs still in use message as warn

--- aws/ebs.go
+++ aws/ebs.go
@@ -48,7 +48,7 @@ func nukeAllEbsVolumes(session *session.Session, volumeIds []*string) error {
 		_, err := svc.DeleteVolume(params)
 		if err != nil {
 			if awsErr, isAwsErr := err.(awserr.Error); isAwsErr && awsErr.Code() == "VolumeInUse" {
-				logging.Logger.Infof("EBS volume %s is attached to an active resource", *volumeID)
+				logging.Logger.Warnf("EBS volume %s can't be deleted, it is still attached to an active resource", *volumeID)
 				return nil
 			} else if awsErr, isAwsErr := err.(awserr.Error); isAwsErr && awsErr.Code() == "InvalidVolume.NotFound" {
 				logging.Logger.Infof("EBS volume %s has already been deleted", *volumeID)
