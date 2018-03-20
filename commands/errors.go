fa0a13e541d3d44106e1f341e1987e930eabaed9
Oluwatoni Solarin-Sodara <toni.edward@outlook.com>
2018-03-20 13:32:59 +0100

    update cli entrypoint to accept cloud providers as arguments

--- commands/errors.go
+++ commands/errors.go
@@ -12,3 +12,15 @@ type InvalidFlagError struct {
 func (e InvalidFlagError) Error() string {
 	return fmt.Sprintf("Invalid value %s for flag %s", e.Value, e.Name)
 }
+
+type UnsupportedProviderError struct {
+	Name string
+}
+
+func (e UnsupportedProviderError) Error() string {
+	if e.Name == "" {
+		return fmt.Sprintf("Invalid cloud provider specified. Possible values aws | azure | gcp")
+	}
+
+	return fmt.Sprintf("%s is not currently supported by cloud-nuke", e.Name)
+}
