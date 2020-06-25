8aa24ff7639829f61c21056934878ac252846273
Ina Stoyanova <ina.m.stoyanova@gmail.com>
2020-06-25 15:45:03 +0100

    adding first test - working by default as expected

--- aws/ecs_cluster_test.go
+++ aws/ecs_cluster_test.go
@@ -1,14 +1,42 @@
 package aws
 
 import (
+	"fmt"
 	"testing"
+
+	awsgo "github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/gruntwork-io/gruntwork-cli/errors"
+	"github.com/stretchr/testify/assert"
 )
 
 // Test that we can succesfully list ECS clusters by manually creating a cluster and then using the list function to find it.
 func TestListEcsClusters(t *testing.T) {
 	// create ECS cluster in a specific region
+	t.Parallel()
+
+	region := "eu-west-1"
+	awsSession, err := session.NewSession(&awsgo.Config{
+		Region: awsgo.String(region),
+	})
+
+	if err != nil {
+		assert.Fail(t, errors.WithStackTrace(err).Error())
+	}
+
+	clusterName := "test-ina-2"
+	cluster := createEcsFargateCluster(t, awsSession, clusterName)
+	defer deleteEcsCluster(awsSession, cluster)
+	//END prepare resources
+
 	// call api to list all clusters
+	clusterArns, err := getAllEcsClusters(awsSession)
+	if err != nil {
+		assert.Failf(t, "Unable to fetch clusters: %s", err.Error())
+	}
+
 	// assert that result contains your new cluster
+	assert.Contains(t, clusterArns, cluster.ClusterArn)
 }
 
 // Test that we can filter ECS clusters by 'created_at' tag value.
