5331531830b53bdf346324c6f3e853a7182d6970
Ina Stoyanova <ina.m.stoyanova@gmail.com>
2020-06-25 15:45:03 +0100

    adding first test - working by default as expected

--- aws/ecs_utils_for_test.go
+++ aws/ecs_utils_for_test.go
@@ -8,6 +8,7 @@ import (
 	"testing"
 	"time"
 
+	"github.com/aws/aws-sdk-go/aws"
 	awsgo "github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/ec2"
