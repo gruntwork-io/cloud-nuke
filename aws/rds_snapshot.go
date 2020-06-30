07f2d0c3ac54e18731d98029e8f8d268336bf527
alexkayabula <alexkayabula@gmail.com>
2020-06-30 16:28:29 +0300

    nuke all snapshots

--- aws/rds_snapshot.go
+++ aws/rds_snapshot.go
@@ -29,3 +29,51 @@ func getAllRdsSnapshots(session *session.Session, excludeAfter time.Time) ([]*st
 
 	return snapshots, nil
 }
+
+func nukeAllRdsSnapshots(session *session.Session, snapshots []*string) error {
+	svc := rds.New(session)
+
+	if len(snapshots) == 0 {
+		logging.Logger.Infof("No RDS Snapshot to nuke in region %s", *session.Config.Region)
+		return nil
+	}
+
+	logging.Logger.Infof("Deleting all RDS Snapshots in region %s", *session.Config.Region)
+	deletedSnapShots := []*string{}
+
+	for _, snapshot := range snapshots {
+		params := &rds.DeleteDBSnapshotInput{
+			DBSnapshotIdentifier: snapshot,
+		}
+
+		_, err := svc.DeleteDBSnapshot(params)
+
+		if err != nil {
+			logging.Logger.Errorf("[Failed] %s: %s", *snapshot, err)
+		} else {
+			deletedSnapShots = append(deletedSnapShots, snapshot)
+			logging.Logger.Infof("Deleted RDS Snapshot: %s", awsgo.StringValue(snapshot))
+		}
+	}
+
+	if len(deletedSnapShots) > 0 {
+		for _, snapshot := range deletedSnapShots {
+
+			err := svc.WaitUntilDBSnapshotDeleted(&rds.DescribeDBSnapshotsInput{
+				DBSnapshotsIdentifier: snapshot,
+			})
+
+			if err != nil {
+				logging.Logger.Errorf("[Failed] %s", err)
+				return errors.WithStackTrace(err)
+			}
+		}
+	}
+
+	if deletedSnapShots != snapshots {
+		logging.Logger.Errorf("[Failed] - %d/%d - RDS Snapshot(s) failed deletion in %s", snapshots-deletedSnapShots, snapshots, *session.Config.Region)
+	}
+
+	logging.Logger.Infof("[OK] %d RDS DB Snapshot(s) deleted in %s", len(deletedSnapShots), *session.Config.Region)
+	return nil
+}
