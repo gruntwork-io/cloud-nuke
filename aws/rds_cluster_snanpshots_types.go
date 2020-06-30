44ffd7dc9ff92c975246dcfb3f65e1175420cb15
alexkayabula <alexkayabula@gmail.com>
2020-06-30 21:01:29 +0300

    nuke all rds cluster snapshots

---
+++ aws/rds_cluster_snanpshots_types.go
@@ -0,0 +1,35 @@
+package aws
+
+import (
+	awsgo "github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/gruntwork-io/gruntwork-cli/errors"
+)
+
+type DBClusterSnapshots struct {
+	SnapShots []string
+}
+
+// Name of the AWS resource
+func (snapshot DBClusterSnapshots) ResourceName() string {
+	return "rds-snapshots"
+}
+
+// Snapshot names of the RDS Cluster Snapshots
+func (snapshot DBClusterSnapshots) ResourceIdentifiers() []string {
+	return snapshot.SnapShots
+}
+
+// MaxBatchSize decides how many snapshots to delete in one call.
+func (snapshot DBClusterSnapshots) MaxBatchSize() int {
+	return 200
+}
+
+//Nuke/Delete all snapshots
+func (snapshot DBClusterSnapshots) Nuke(session *session.Session, identifiers []string) error {
+	if err := nukeAllRdsSnapshots(session, awsgo.StringSlice(identifiers)); err != nil {
+		return errors.WithStackTrace(err)
+	}
+
+	return nil
+}
