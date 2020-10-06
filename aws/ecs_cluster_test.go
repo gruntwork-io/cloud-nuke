6a6ae4a4d62f77fcff27e8fb87afc86e34f9b5c5
Ina Stoyanova <ina.m.stoyanova@gmail.com>
2020-10-06 15:37:41 +0100

    updating test for filtering ECS clusters to tag clusters with appropriate timestamps

--- aws/ecs_cluster_test.go
+++ aws/ecs_cluster_test.go
@@ -58,9 +58,14 @@ func TestCanTagEcsClusters(t *testing.T) {
 	assert.Equal(t, returnedTag.Format(time.RFC3339), tagValue)
 }
 
-// Test that we can filter ECS clusters by 'created_at' tag value.
-func TestCanFilterOlderEcsClusters(t *testing.T) {
+// Test we can get all ECS clusters younger than < X time based on tags
+func TestCanListAllEcsClustersOlderThan24hours(t *testing.T) {
 	t.Parallel()
+	// create 3 clusters with tags: 1hr, 22hrs, 28hrs
+	// get all ecs clusters
+	// get tags for each cluster
+	// select only clusters older than 24hrs
+	// assert return only 1 cluster
 
 	region := "eu-west-1"
 	awsSession, err := session.NewSession(&awsgo.Config{
@@ -68,22 +73,24 @@ func TestCanFilterOlderEcsClusters(t *testing.T) {
 	})
 	require.NoError(t, err)
 
-	clusterName := util.UniqueID()
-	cluster := createEcsFargateCluster(t, awsSession, clusterName)
-	defer deleteEcsCluster(awsSession, cluster)
+	cluster1 := createEcsFargateCluster(t, awsSession, util.UniqueID())
+	// defer deleteEcsCluster(awsSession, cluster1)
+	cluster2 := createEcsFargateCluster(t, awsSession, util.UniqueID())
+	// defer deleteEcsCluster(awsSession, cluster2)
+	cluster3 := createEcsFargateCluster(t, awsSession, util.UniqueID())
+	// defer deleteEcsCluster(awsSession, cluster3)
 
-	tagEcsCluster(awsSession, cluster.ClusterArn, tagKey, time.Now().UTC().String())
+	var twoHoursOldTagValue = time.Now().Add(time.Hour * time.Duration(-2)).Format(time.RFC3339)
+	var nowTagValue = time.Now().Format(time.RFC3339)
+	var twentyFiveHoursOldTagValue = time.Now().Add(time.Hour * time.Duration(-25)).Format(time.RFC3339)
+
+	tagEcsCluster(awsSession, cluster1.ClusterArn, tagKey, twoHoursOldTagValue)
+	tagEcsCluster(awsSession, cluster2.ClusterArn, tagKey, nowTagValue)
+	tagEcsCluster(awsSession, cluster3.ClusterArn, tagKey, twentyFiveHoursOldTagValue)
 	require.NoError(t, err)
 
 }
 
-// Test we can get all ECS clusters younger than < X time based on tags
-func TestCanListAllEcsClustersOlderThan24hours(t *testing.T) {
-	// create 3 clusters with tags: 1hr, 22hrs, 28hrs
-	// get all ecs clusters
-	// get tags for each cluster
-	// select only clusters older than 24hrs
-	// assert return only 1 cluster
 }
 
 // Test we can nuke all ECS clusters younger than < X time
@@ -95,4 +102,12 @@ func TestCanNukeAllEcsClustersOlderThan24Hours(t *testing.T) {
 	// nuke selected clusters
 	// assert 2 clusters nuked
 	// assert 1 cluster still left
+
+	// nuked, failed := nukeEcsClusters(awsSession, clusterArns)
+	// fmt.Println(nuked)
+	// fmt.Println(failed)
+	// require.NoError(t, err)
+
+	// assert.Equal(t, 1, len(nuked))
+	// assert.Equal(t, 0, len(failed))
 }
