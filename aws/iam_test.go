3e1190a3519f86a1215661c4d9727d4114b49783
Hugo Posca <hugo.posca@gusto.com>
2020-12-08 11:59:36 -0800

    Create test IAM User
    - Created a function that creates an IAM user for testing purposes
    - Tests that the user was actually created
    - Later, will nuke this user

--- aws/iam_test.go
+++ aws/iam_test.go
@@ -4,8 +4,12 @@ import (
 	"fmt"
 	"testing"
 
+	"github.com/aws/aws-sdk-go/aws"
 	awsgo "github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/iam"
+	"github.com/gruntwork-io/cloud-nuke/util"
+	"github.com/gruntwork-io/gruntwork-cli/errors"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
@@ -32,4 +36,42 @@ func TestListIamUsers(t *testing.T) {
 	}
 
 	assert.NotEmpty(t, userNames)
+}
+
+func createTestUser(t *testing.T, session *session.Session, name string) error {
+	svc := iam.New(session)
+	input := &iam.CreateUserInput{
+		UserName: aws.String(name),
+	}
+
+	_, err := svc.CreateUser(input)
+	if err != nil {
+		return errors.WithStackTrace(err)
+	}
+
+	return nil
+}
+
+func TestCreateIamUser(t *testing.T) {
+	t.Parallel()
+
+	region, err := getRandomRegion()
+	require.NoError(t, err)
+
+	session, err := session.NewSession(&awsgo.Config{
+		Region: awsgo.String(region)},
+	)
+	require.NoError(t, err)
+
+	name := "cloud-nuke-test-" + util.UniqueID()
+	userNames, err := getAllIamUsers(session, region)
+	require.NoError(t, err)
+	assert.NotContains(t, awsgo.StringValueSlice(userNames), name)
+
+	err = createTestUser(t, session, name)
+	require.NoError(t, err)
+
+	userNames, err = getAllIamUsers(session, region)
+	require.NoError(t, err)
+	assert.Contains(t, awsgo.StringValueSlice(userNames), name)
 }
