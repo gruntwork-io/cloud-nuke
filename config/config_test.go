dd5539680599e3ecfc11dbaf7c7442def4beb811
HebertCL <hebert.cuellar@gmail.com>
2020-12-10 12:08:48 -0600

    Updated tests with coverage for IAM Role and its respective mock files

--- config/config_test.go
+++ config/config_test.go
@@ -25,15 +25,6 @@ func TestConfig_Garbage(t *testing.T) {
 	return
 }
 
-func TestConfig_Malformed(t *testing.T) {
-	configFilePath := "./mocks/malformed.yaml"
-	_, err := GetConfig(configFilePath)
-
-	// Expect malformed to throw a yaml TypeError
-	require.Error(t, err, "Received expected error")
-	return
-}
-
 func TestConfig_Empty(t *testing.T) {
 	configFilePath := "./mocks/empty.yaml"
 	configObj, err := GetConfig(configFilePath)
@@ -47,6 +38,23 @@ func TestConfig_Empty(t *testing.T) {
 	return
 }
 
+func TestConfigS3_Malformed(t *testing.T) {
+	configFilePath := "./mocks/s3_malformed.yaml"
+	_, err := GetConfig(configFilePath)
+
+	// Expect malformed to throw a yaml TypeError
+	require.Error(t, err, "Received expected error")
+	return
+}
+
+func TestConfigIamRole_Malformed(t *testing.T) {
+	configPath := "./mocks/iamRole_malformed.yaml"
+	_, err := GetConfig(configPath)
+
+	require.Error(t, err, "Received expected error")
+	return
+}
+
 func TestConfigS3_Empty(t *testing.T) {
 	configFilePath := "./mocks/s3_empty.yaml"
 	configObj, err := GetConfig(configFilePath)
@@ -55,6 +63,19 @@ func TestConfigS3_Empty(t *testing.T) {
 
 	if !reflect.DeepEqual(configObj, emptyConfig()) {
 		assert.Fail(t, "Config should be empty, %+v\n", configObj.S3)
+	}
+
+	return
+}
+
+func TestConfigIamRole_Empty(t *testing.T) {
+	configFilePath := "./mocks/iamRole_empty.yaml"
+	configObj, err := GetConfig(configFilePath)
+
+	require.NoError(t, err)
+
+	if !reflect.DeepEqual(configObj, emptyConfig()) {
+		assert.Fail(t, "Config should be empty, %+v\n", configObj.IamRole)
 	}
 
 	return
@@ -73,8 +94,34 @@ func TestConfigS3_EmptyFilters(t *testing.T) {
 	return
 }
 
+func TestConfigIamRole_EmptyFilters(t *testing.T) {
+	configFilePath := "./mocks/iamRole_empty_filters.yaml"
+	configObj, err := GetConfig(configFilePath)
+
+	require.NoError(t, err)
+
+	if !reflect.DeepEqual(configObj, emptyConfig()) {
+		assert.Fail(t, "Config should be empty, %+v\n", configObj)
+	}
+
+	return
+}
+
 func TestConfigS3_EmptyRules(t *testing.T) {
 	configFilePath := "./mocks/s3_empty_rules.yaml"
+	configObj, err := GetConfig(configFilePath)
+
+	require.NoError(t, err)
+
+	if !reflect.DeepEqual(configObj, emptyConfig()) {
+		assert.Fail(t, "Config should be empty, %+v\n", configObj)
+	}
+
+	return
+}
+
+func TestConfigIamRole_EmptyRules(t *testing.T) {
+	configFilePath := "./mocks/iamRole_empty_rules.yaml"
 	configObj, err := GetConfig(configFilePath)
 
 	require.NoError(t, err)
@@ -103,6 +150,23 @@ func TestConfigS3_IncludeNames(t *testing.T) {
 	return
 }
 
+func TestConfigIamRole_IncludeNames(t *testing.T) {
+	configFilePath := "./mocks/iamRole_include_names.yaml"
+	configObj, err := GetConfig(configFilePath)
+
+	require.NoError(t, err)
+
+	if reflect.DeepEqual(configObj, emptyConfig()) {
+		assert.Fail(t, "Config should not be empty, %+v\n", configObj)
+	}
+
+	if len(configObj.IamRole.IncludeRule.NamesRE) == 0 {
+		assert.Fail(t, "ConfigObj should contain IAM Role names regexes, %+v\n", configObj)
+	}
+
+	return
+}
+
 func TestConfigS3_ExcludeNames(t *testing.T) {
 	configFilePath := "./mocks/s3_exclude_names.yaml"
 	configObj, err := GetConfig(configFilePath)
@@ -115,6 +179,23 @@ func TestConfigS3_ExcludeNames(t *testing.T) {
 
 	if len(configObj.S3.ExcludeRule.NamesRE) == 0 {
 		assert.Fail(t, "ConfigObj should contain S3 names regexes, %+v\n", configObj)
+	}
+
+	return
+}
+
+func TestConfigIamRole_ExcludeNames(t *testing.T) {
+	configFilePath := "./mocks/iamRole_exclude_names.yaml"
+	configObj, err := GetConfig(configFilePath)
+
+	require.NoError(t, err)
+
+	if reflect.DeepEqual(configObj, emptyConfig()) {
+		assert.Fail(t, "Config should not be empty, %+v\n", configObj)
+	}
+
+	if len(configObj.IamRole.ExcludeRule.NamesRE) == 0 {
+		assert.Fail(t, "ConfigObj should contain IAM Role names regexes, %+v\n", configObj)
 	}
 
 	return
@@ -133,6 +214,44 @@ func TestConfigS3_FilterNames(t *testing.T) {
 	if len(configObj.S3.IncludeRule.NamesRE) == 0 ||
 		len(configObj.S3.ExcludeRule.NamesRE) == 0 {
 		assert.Fail(t, "ConfigObj should contain S3 names regexes, %+v\n", configObj)
+	}
+
+	return
+}
+
+func TestConfigIamRole_FilterNames(t *testing.T) {
+	configFilePath := "./mocks/iamRole_filter_names.yaml"
+	configObj, err := GetConfig(configFilePath)
+
+	require.NoError(t, err)
+
+	if reflect.DeepEqual(configObj, emptyConfig()) {
+		assert.Fail(t, "Config should not be empty, %+v\n", configObj)
+	}
+
+	if len(configObj.IamRole.IncludeRule.NamesRE) == 0 ||
+		len(configObj.IamRole.ExcludeRule.NamesRE) == 0 {
+		assert.Fail(t, "ConfigObj should contain IAM Role names regexes, %+v\n", configObj)
+	}
+
+	return
+}
+
+func TestConfig_MixedConfig(t *testing.T) {
+	configFilePath := "./mocks/mixedConfig.yaml"
+	configObj, err := GetConfig(configFilePath)
+
+	require.NoError(t, err)
+
+	if reflect.DeepEqual(configObj, emptyConfig()) {
+		assert.Fail(t, "Config should not be empty, %+v\n", configObj)
+	}
+
+	if len(configObj.S3.IncludeRule.NamesRE) == 0 ||
+		len(configObj.S3.ExcludeRule.NamesRE) == 0 ||
+		len(configObj.IamRole.IncludeRule.NamesRE) == 0 ||
+		len(configObj.IamRole.ExcludeRule.NamesRE) == 0 {
+		assert.Fail(t, "ConfigObj should contain both IAM Role and S3 names regexes, %+v\n", configObj)
 	}
 
 	return
