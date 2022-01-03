8de94a68f1020bf89d2195f3428237d21639e213
yorinasub17 <430092+yorinasub17@users.noreply.github.com>
2022-01-03 14:46:31 -0600

    Fix build failure

--- config/config_test.go
+++ config/config_test.go
@@ -23,6 +23,7 @@ func emptyConfig() *Config {
 		ResourceType{FilterRule{}, FilterRule{}},
 		ResourceType{FilterRule{}, FilterRule{}},
 		ResourceType{FilterRule{}, FilterRule{}},
+		ResourceType{FilterRule{}, FilterRule{}},
 	}
 }
 
