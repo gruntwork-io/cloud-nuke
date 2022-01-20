4b341ca0134366e7ba595d91acdd0c00df7557e3
Toni Solarin-Sodara <toni.edward@outlook.com>
2022-01-20 14:47:19 -0800

    Add --older-than support to VPC (#272)
    * Add functions to get and set VPC tags
    
    * Consider first seen tag when listing VPCs
    
    * Add vpc tag test
    
    * Add logging when we fail to retrieve tags
    
    * Fix existing VPC tests
    
    * Missed one!

--- aws/ec2_vpc_test.go
+++ aws/ec2_vpc_test.go
@@ -3,6 +3,7 @@ package aws
 import (
 	"regexp"
 	"testing"
+	"time"
 
 	awsgo "github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
@@ -28,6 +29,60 @@ func createTestVpc(t *testing.T, session *session.Session) string {
 	return *vpc.Vpc.VpcId
 }
 
+func TestCanTagVpc(t *testing.T) {
+	t.Parallel()
+
+	region, err := getRandomRegion()
+	require.NoError(t, err)
+
+	session, err := session.NewSession(&awsgo.Config{
+		Region: awsgo.String(region)},
+	)
+
+	require.NoError(t, err)
+
+	vpcId := createTestVpc(t, session)
+	svc := ec2.New(session)
+
+	// clean up after this test
+	defer nukeAllVPCs(session, []string{vpcId}, []Vpc{{
+		Region: region,
+		VpcId:  vpcId,
+		svc:    svc,
+	}})
+
+	result, err := svc.DescribeVpcs(&ec2.DescribeVpcsInput{
+		VpcIds: awsgo.StringSlice([]string{vpcId}),
+	})
+
+	require.NoError(t, err)
+	require.Equal(t, 1, len(result.Vpcs))
+
+	vpc := result.Vpcs[0]
+	key := "cloud-nuke-first-seen-test"
+	value := time.Now().UTC()
+
+	err = setFirstSeenVpcTag(svc, *vpc, key, value)
+	require.NoError(t, err)
+
+	result, err = svc.DescribeVpcs(&ec2.DescribeVpcsInput{
+		VpcIds: awsgo.StringSlice([]string{vpcId}),
+	})
+
+	require.NoError(t, err)
+
+	vpc = result.Vpcs[0]
+	value1, err := getFirstSeenVpcTag(*vpc, key)
+	require.NoError(t, err)
+	require.NotNil(t, value1)
+
+	// Parsing from string doesn't include the millisecond,
+	// so format the dates according to this layout so we can
+	// perform a direct comparison.
+	layout := "2006-01-02T15:04:05"
+	assert.Equal(t, value.Format(layout), (*value1).Format(layout))
+}
+
 func TestListVpcs(t *testing.T) {
 	t.Parallel()
 
@@ -49,7 +104,12 @@ func TestListVpcs(t *testing.T) {
 		svc:    ec2.New(session),
 	}})
 
-	vpcIds, _, err := getAllVpcs(session, region, config.Config{})
+	// First run gives us a chance to tag the VPC
+	_, _, err = getAllVpcs(session, region, time.Now().Add(1*time.Hour), config.Config{})
+	require.NoError(t, err)
+
+	// VPC should be tagged at this point
+	vpcIds, _, err := getAllVpcs(session, region, time.Now().Add(1*time.Hour), config.Config{})
 	require.NoError(t, err)
 
 	assert.Contains(t, awsgo.StringValueSlice(vpcIds), vpcId)
@@ -81,7 +141,8 @@ func TestListVpcsWithConfigFile(t *testing.T) {
 		svc:    ec2.New(session),
 	}})
 
-	vpcIds, _, err := getAllVpcs(session, region, config.Config{
+	// First run gives us a chance to tag the VPC
+	_, _, err = getAllVpcs(session, region, time.Now().Add(1*time.Hour), config.Config{
 		VPC: config.ResourceType{
 			IncludeRule: config.FilterRule{
 				NamesRegExp: []config.Expression{
@@ -92,6 +153,20 @@ func TestListVpcsWithConfigFile(t *testing.T) {
 	})
 
 	require.NoError(t, err)
+
+	// VPC should be tagged at this point
+	vpcIds, _, err := getAllVpcs(session, region, time.Now().Add(1*time.Hour), config.Config{
+		VPC: config.ResourceType{
+			IncludeRule: config.FilterRule{
+				NamesRegExp: []config.Expression{
+					{RE: *regexp.MustCompile(includedVpcId)},
+				},
+			},
+		},
+	})
+
+	require.NoError(t, err)
+
 	require.Equal(t, 1, len(vpcIds))
 	assert.Contains(t, awsgo.StringValueSlice(vpcIds), includedVpcId)
 }
@@ -119,7 +194,12 @@ func TestNukeVpcs(t *testing.T) {
 
 	require.NoError(t, err)
 
-	vpcIds, _, err := getAllVpcs(session, region, config.Config{})
+	// First run gives us a chance to tag the VPC
+	_, _, err = getAllVpcs(session, region, time.Now().Add(1*time.Hour), config.Config{})
+	require.NoError(t, err)
+
+	// VPC should be tagged at this point
+	vpcIds, _, err := getAllVpcs(session, region, time.Now().Add(1*time.Hour), config.Config{})
 	require.NoError(t, err)
 
 	assert.NotContains(t, awsgo.StringValueSlice(vpcIds), vpcId)
