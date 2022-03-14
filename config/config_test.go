bb49340d57b125b2a1ebcea01aab572cf87468bb
Denis O <denis.o@linux.com>
2022-03-14 23:23:45 +0200

    Updated empty config creation

--- config/config_test.go
+++ config/config_test.go
@@ -28,6 +28,7 @@ func emptyConfig() *Config {
 		ResourceType{FilterRule{}, FilterRule{}},
 		ResourceType{FilterRule{}, FilterRule{}},
 		ResourceType{FilterRule{}, FilterRule{}},
+		ResourceType{FilterRule{}, FilterRule{}},
 	}
 }
 
