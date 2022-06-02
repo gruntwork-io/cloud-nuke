ca567b6e2d585f48e268045f1e336204a1170a59
Zachary Proser <zackproser@gmail.com>
2022-06-02 14:59:36 -0400

    Update NewQuery calls in tests

--- aws/types_test.go
+++ aws/types_test.go
@@ -12,6 +12,7 @@ func TestNewQueryRejectsInvalidExcludeAfterEntries(t *testing.T) {
 	type TestCase struct {
 		Name                 string
 		Regions              []string
+		ExcludeRegions       []string
 		ResourceTypes        []string
 		ExcludeResourceTypes []string
 		ExcludeAfter         string
@@ -22,6 +23,7 @@ func TestNewQueryRejectsInvalidExcludeAfterEntries(t *testing.T) {
 		{
 			Name:                 "Invalid excludeAfter time string rejected",
 			Regions:              []string{"us-east-1"},
+			ExcludeRegions:       []string{},
 			ResourceTypes:        []string{"ec2", "vpc"},
 			ExcludeResourceTypes: []string{},
 			ExcludeAfter:         "this is not a valid time duration",
@@ -30,6 +32,7 @@ func TestNewQueryRejectsInvalidExcludeAfterEntries(t *testing.T) {
 		{
 			Name:                 "Empty excludeAfter time string rejected",
 			Regions:              []string{"us-east-1"},
+			ExcludeRegions:       []string{},
 			ResourceTypes:        []string{"ec2", "vpc"},
 			ExcludeResourceTypes: []string{},
 			ExcludeAfter:         "", // this ExcludeAfter is intentionally left empty
@@ -38,7 +41,7 @@ func TestNewQueryRejectsInvalidExcludeAfterEntries(t *testing.T) {
 	}
 	for _, tc := range testCases {
 		t.Run(tc.Name, func(t *testing.T) {
-			_, err := NewQuery(tc.Regions, tc.ResourceTypes, tc.ExcludeResourceTypes, tc.ExcludeAfter)
+			_, err := NewQuery(tc.Regions, tc.ExcludeRegions, tc.ResourceTypes, tc.ExcludeResourceTypes, tc.ExcludeAfter)
 			if !errors.As(err, &tc.Error) {
 				t.Logf("%s: Expected error of type %T but got %T", tc.Name, tc.Error, err)
 				t.Fail()
@@ -51,6 +54,7 @@ func TestNewQueryAcceptsValidExcludeAfterEntries(t *testing.T) {
 	type TestCase struct {
 		Name                 string
 		Regions              []string
+		ExcludeRegions       []string
 		ResourceTypes        []string
 		ExcludeResourceTypes []string
 		ExcludeAfter         string
@@ -58,22 +62,24 @@ func TestNewQueryAcceptsValidExcludeAfterEntries(t *testing.T) {
 
 	testCases := []TestCase{
 		{
-			Name:          "Can pass simple durations: 1h",
-			Regions:       []string{"us-east-1"},
-			ResourceTypes: []string{"ec2"},
-			ExcludeAfter:  "1h",
+			Name:           "Can pass simple durations: 1h",
+			Regions:        []string{"us-east-1"},
+			ExcludeRegions: []string{},
+			ResourceTypes:  []string{"ec2"},
+			ExcludeAfter:   "1h",
 		},
 		{
-			Name:          "Can pass simple durations: 65m",
-			Regions:       []string{"us-east-1"},
-			ResourceTypes: []string{"ec2"},
-			ExcludeAfter:  "65m",
+			Name:           "Can pass simple durations: 65m",
+			Regions:        []string{"us-east-1"},
+			ExcludeRegions: []string{},
+			ResourceTypes:  []string{"ec2"},
+			ExcludeAfter:   "65m",
 		},
 	}
 
 	for _, tc := range testCases {
 		t.Run(tc.Name, func(t *testing.T) {
-			_, err := NewQuery(tc.Regions, tc.ResourceTypes, tc.ExcludeResourceTypes, tc.ExcludeAfter)
+			_, err := NewQuery(tc.Regions, tc.ExcludeRegions, tc.ResourceTypes, tc.ExcludeResourceTypes, tc.ExcludeAfter)
 			require.NoError(t, err)
 		})
 	}
