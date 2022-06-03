e9428bb6aec7ba6cf8705f8ab9558ff44b76b028
Zachary Proser <zackproser@gmail.com>
2022-06-03 11:35:22 -0400

    Update test with new ExcludeAfter signature

--- aws/types_test.go
+++ aws/types_test.go
@@ -1,54 +1,11 @@
 package aws
 
 import (
-	"errors"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/require"
 )
-
-func TestNewQueryRejectsInvalidExcludeAfterEntries(t *testing.T) {
-
-	type TestCase struct {
-		Name                 string
-		Regions              []string
-		ExcludeRegions       []string
-		ResourceTypes        []string
-		ExcludeResourceTypes []string
-		ExcludeAfter         string
-		Error                InvalidTimeStringPassedError
-	}
-
-	testCases := []TestCase{
-		{
-			Name:                 "Invalid excludeAfter time string rejected",
-			Regions:              []string{"us-east-1"},
-			ExcludeRegions:       []string{},
-			ResourceTypes:        []string{"ec2", "vpc"},
-			ExcludeResourceTypes: []string{},
-			ExcludeAfter:         "this is not a valid time duration",
-			Error:                InvalidTimeStringPassedError{},
-		},
-		{
-			Name:                 "Empty excludeAfter time string rejected",
-			Regions:              []string{"us-east-1"},
-			ExcludeRegions:       []string{},
-			ResourceTypes:        []string{"ec2", "vpc"},
-			ExcludeResourceTypes: []string{},
-			ExcludeAfter:         "", // this ExcludeAfter is intentionally left empty
-			Error:                InvalidTimeStringPassedError{},
-		},
-	}
-	for _, tc := range testCases {
-		t.Run(tc.Name, func(t *testing.T) {
-			_, err := NewQuery(tc.Regions, tc.ExcludeRegions, tc.ResourceTypes, tc.ExcludeResourceTypes, tc.ExcludeAfter)
-			if !errors.As(err, &tc.Error) {
-				t.Logf("%s: Expected error of type %T but got %T", tc.Name, tc.Error, err)
-				t.Fail()
-			}
-		})
-	}
-}
 
 func TestNewQueryAcceptsValidExcludeAfterEntries(t *testing.T) {
 	type TestCase struct {
@@ -57,23 +14,23 @@ func TestNewQueryAcceptsValidExcludeAfterEntries(t *testing.T) {
 		ExcludeRegions       []string
 		ResourceTypes        []string
 		ExcludeResourceTypes []string
-		ExcludeAfter         string
+		ExcludeAfter         time.Time
 	}
 
 	testCases := []TestCase{
 		{
-			Name:           "Can pass simple durations: 1h",
+			Name:           "Can pass time.Now plus 24 hours",
 			Regions:        []string{"us-east-1"},
 			ExcludeRegions: []string{},
 			ResourceTypes:  []string{"ec2"},
-			ExcludeAfter:   "1h",
+			ExcludeAfter:   time.Now().Add(time.Hour * 24),
 		},
 		{
-			Name:           "Can pass simple durations: 65m",
+			Name:           "Can pass time.Now",
 			Regions:        []string{"us-east-1"},
 			ExcludeRegions: []string{},
 			ResourceTypes:  []string{"ec2"},
-			ExcludeAfter:   "65m",
+			ExcludeAfter:   time.Now(),
 		},
 	}
 
