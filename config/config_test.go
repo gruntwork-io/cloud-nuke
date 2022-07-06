87a4055beacfdc021da26f4c6211d6cbfcf40c24
Zachary Proser <zackproser@gmail.com>
2022-07-06 10:55:45 -0400

    Remove excess ResourceType entry

--- config/config_test.go
+++ config/config_test.go
@@ -34,7 +34,6 @@ func emptyConfig() *Config {
 		ResourceType{FilterRule{}, FilterRule{}},
 		ResourceType{FilterRule{}, FilterRule{}},
 		ResourceType{FilterRule{}, FilterRule{}},
-		ResourceType{FilterRule{}, FilterRule{}},
 	}
 }
 
