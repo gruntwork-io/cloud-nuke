9342b205267a6f29425519a537d745d38e55302d
Yoriyasu Yano <430092+yorinasub17@users.noreply.github.com>
2022-07-19 11:25:15 -0500

    Fix build failure on config

--- config/config_test.go
+++ config/config_test.go
@@ -35,6 +35,7 @@ func emptyConfig() *Config {
 		ResourceType{FilterRule{}, FilterRule{}},
 		ResourceType{FilterRule{}, FilterRule{}},
 		ResourceType{FilterRule{}, FilterRule{}},
+		ResourceType{FilterRule{}, FilterRule{}},
 	}
 }
 
