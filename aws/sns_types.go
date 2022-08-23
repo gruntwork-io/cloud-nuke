04b0bb1e7a3bc44bfe00b9f9f490bf15b350ee94
Zack Proser <zackproser@gmail.com>
2022-08-23 17:22:44 -0400

    Implement support for SNS Topics (#354)

---
+++ aws/sns_types.go
@@ -0,0 +1,38 @@
+package aws
+
+import (
+	awsgo "github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/gruntwork-io/go-commons/errors"
+)
+
+type SNSTopic struct {
+	Arns []string
+}
+
+func (s SNSTopic) ResourceName() string {
+	return "snstopic"
+}
+
+func (s SNSTopic) ResourceIdentifiers() []string {
+	return s.Arns
+}
+
+func (s SNSTopic) MaxBatchSize() int {
+	return 50
+}
+
+func (s SNSTopic) Nuke(session *session.Session, identifiers []string) error {
+	if err := nukeAllSNSTopics(session, awsgo.StringSlice(identifiers)); err != nil {
+		return errors.WithStackTrace(err)
+	}
+	return nil
+}
+
+// custom errors
+
+type TooManySNSTopicsErr struct{}
+
+func (err TooManySNSTopicsErr) Error() string {
+	return "Too many SNS Topics requested at once."
+}
