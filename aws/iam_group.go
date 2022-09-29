b894315b20393e4a95eee06fa92880b2ed9ce9fb
Andrew Ellison <ellisonc@wustl.edu>
2022-09-29 14:21:52 -0500

    116 - WIP - write tests for iam_group

--- aws/iam_group.go
+++ aws/iam_group.go
@@ -3,37 +3,84 @@ package aws
 import (
 	"time"
 
+	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/iam"
 	"github.com/gruntwork-io/cloud-nuke/config"
+	"github.com/gruntwork-io/cloud-nuke/logging"
+	"github.com/gruntwork-io/gruntwork-cli/errors"
 )
 
 func getAllIamGroups(session *session.Session, excludeAfter time.Time, configObj config.Config) ([]*string, error) {
-	//TODO implement
-	return nil, nil
+	svc := iam.New(session)
+
+	allIamGroups := []*string{}
+	err := svc.ListGroupsPages(
+		&iam.ListGroupsInput{},
+		func(page *iam.ListGroupsOutput, lastPage bool) bool {
+			for _, iamGroup := range page.Groups {
+				if shouldIncludeIamGroup(iamGroup, excludeAfter, configObj) {
+					allIamGroups = append(allIamGroups, iamGroup.GroupName)
+				}
+			}
+			return !lastPage
+		},
+	)
+	if err != nil {
+		return nil, errors.WithStackTrace(err)
+	}
+	return allIamGroups, nil
 }
 
-//nukeAllIamGroups - delete all IAM Roles
+//nukeAllIamGroups - delete all IAM Roles.  Caller is responsible for pagination (no more than 100/request)
 func nukeAllIamGroups(session *session.Session, groupNames []*string) error {
+	region := aws.StringValue(session.Config.Region) //Since this is a global resource this can be any random region
+	svc := iam.New(session)
+
+	if len(groupNames) == 0 {
+		logging.Logger.Info("No IAM Groups to nuke")
+		return nil
+	}
+
+	//Probably not required since pagination is handled by the caller
+	if len(groupNames) > 100 {
+		logging.Logger.Errorf("Nuking too many IAM Groups at once (100): Halting to avoid rate limits")
+		return TooManyIamGroupErr{}
+	}
+
+	//No bulk delete exists, do it with goroutines
+	//TODO
+
 	//TODO implement
 	return nil
 }
 
-//deleteIamGroupAsync - Asynchronosly remove an IAM group from AWS along with any sub-items
-func deleteIamGroupAsync() {
-	//TODO implement
-}
-
-//TODO do I need shouldInclude function?
-
-//deleteIamGroup - removes an IAM group from AWS (ensure group is empty before calling?)
+//deleteIamGroup - removes an IAM group from AWS
 func deleteIamGroup(svc *iam.IAM, groupName *string) error {
+	_, err := svc.DeleteGroup(&iam.DeleteGroupInput{
+		GroupName: groupName,
+	})
+	if err != nil {
+		return errors.WithStackTrace(err)
+	}
 	return nil
 }
 
-//Not sure if needed
+//check if iam group should be included based on config rules (RegExp and Exclude After)
 func shouldIncludeIamGroup(iamGroup *iam.Group, excludeAfter time.Time, configObj config.Config) bool {
-	return false
+	if iamGroup == nil {
+		return false
+	}
+
+	if excludeAfter.Before(*iamGroup.CreateDate) {
+		return false
+	}
+
+	return config.ShouldInclude(
+		aws.StringValue(iamGroup.GroupName),
+		configObj.IAMGroups.IncludeRule.NamesRegExp,
+		configObj.IAMGroups.ExcludeRule.NamesRegExp,
+	)
 }
 
 //TODO delete policy functions belong here eventually but out of scope for trial
--- aws/iam_group_test.go
+++ aws/iam_group_test.go
@@ -1,16 +1,20 @@
 package aws
 
 import (
+	"fmt"
 	"testing"
 	"time"
 
 	awsgo "github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/iam"
 	"github.com/gruntwork-io/cloud-nuke/config"
+	"github.com/gruntwork-io/cloud-nuke/util"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
+//Test that we can list IAM groups in an AWS account
 func TestListIamGroups(t *testing.T) {
 	t.Parallel()
 
@@ -24,10 +28,41 @@ func TestListIamGroups(t *testing.T) {
 
 	groupNames, err := getAllIamGroups(session, time.Now(), config.Config{})
 	require.NoError(t, err)
-	assert.NotEmpty(t, groupNames) //TODO based on iam test ask if needs preconfiguration to have at least one group
+	assert.NotEmpty(t, groupNames)
 }
 
-//TODO implement create a new testing group, with users?
+//TODO could create empty and full IAM groups?
+
+//Creates an empty IAM group for testing
 func createTestGroup(t *testing.T, session *session.Session, name string) error {
+	svc := iam.New(session)
+
+	groupInput := &iam.CreateGroupInput{
+		GroupName: awsgo.String(name),
+	}
+
+	group, err := svc.CreateGroup(groupInput)
+	fmt.Println(group.Group.Arn)
+	require.NoError(t, err)
 	return nil
+}
+
+//Test that we can nuke iam groups.
+func TestNukeIamGroups(t *testing.T) {
+	t.Parallel()
+
+	region, err := getRandomRegion()
+	require.NoError(t, err)
+
+	session, err := session.NewSession(&awsgo.Config{
+		Region: awsgo.String(region),
+	})
+	require.NoError(t, err)
+
+	name := "cloud-nuke-test" + util.UniqueID()
+	err = createTestGroup(t, session, name)
+	require.NoError(t, err)
+
+	err = nukeAllIamGroups(session, []*string{&name})
+	require.NoError(t, err)
 }
