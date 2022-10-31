72862d0b7c1d54c7991d61bf93dd312cff9a41e6
Andrew Ellison <andrew@gruntwork.io>
2022-10-31 09:48:19 -0500

    116 - PR fix for broken test config

--- config/config_test.go
+++ config/config_test.go
@@ -40,6 +40,8 @@ func emptyConfig() *Config {
 		ResourceType{FilterRule{}, FilterRule{}},
 		ResourceType{FilterRule{}, FilterRule{}},
 		ResourceType{FilterRule{}, FilterRule{}},
+		ResourceType{FilterRule{}, FilterRule{}},
+		ResourceType{FilterRule{}, FilterRule{}},
 	}
 }
 
