ca817d2b97327d76d03719128a43b39ef8dcbad9
Andrew Ellison <andrew@gruntwork.io>
2023-03-09 11:06:51 -0600

    upgrade to cli v2

--- logging/logger.go
+++ logging/logger.go
@@ -3,4 +3,8 @@ package logging
 import "github.com/gruntwork-io/go-commons/logging"
 
 // Logger - Global logger variable
-var Logger = logging.GetLogger("cloud-nuke")
+var Logger = logging.GetLogger("cloud-nuke", "")
+
+func InitLogger(name string, version string) {
+	Logger = logging.GetLogger(name, version)
+}
