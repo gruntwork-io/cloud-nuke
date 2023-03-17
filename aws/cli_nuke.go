8b3b9834adddb77eda494bc53fefe49496deddf7
Zack Proser <zackproser@gmail.com>
2023-03-17 00:19:55 -0400

    Delete cli_nuke.go

--- aws/cli_nuke.go
+++
@@ -1,7 +0,0 @@
-package aws
-
-func NukeResourcesViaCLI(a *AwsAccountResources, regions []string) error {
-	// Log which resource types will be inspected
-	// NOTE: The inspect functionality currently does not support config file, so we short circuit the logic with an empty struct.
-	return NukeAllResources(a, regions)
-}
