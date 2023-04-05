239549d416346a7529290da7862b40396c74a5fa
Andrew Ellison <andrew@gruntwork.io>
2023-04-05 14:48:13 -0500

    Fix tests (#437)
    * Fix VPC testing order
    
    * Fix config test

--- config/config_test.go
+++ config/config_test.go
@@ -51,6 +51,7 @@ func emptyConfig() *Config {
 		ResourceType{FilterRule{}, FilterRule{}},
 		ResourceType{FilterRule{}, FilterRule{}},
 		ResourceType{FilterRule{}, FilterRule{}},
+		ResourceType{FilterRule{}, FilterRule{}},
 	}
 }
 
