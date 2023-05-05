f3f05ccdeee2574a6fb1b4bf606a5a1b4e1985d6
arsci <ryan@gruntwork.io>
2023-05-05 15:54:19 -0700

    refactor macie

--- aws/macie.go
+++ aws/macie.go
@@ -20,7 +20,7 @@ import (
 // Unfortunately, the Macie API doesn't provide the metadata information we'd need to implement the configObj pattern, so we
 // currently can only accept a session and excludeAfter
 
-func getMacieStatus(session *session.Session, excludeAfter time.Time) ([]string, error) {
+func getMacie(session *session.Session, excludeAfter time.Time) ([]string, error) {
 	svc := macie2.New(session)
 	var macieStatus []string
 
@@ -75,74 +75,27 @@ func getAllMacieMembers(svc *macie2.Macie2) ([]*string, error) {
 	return memberAccountIds, nil
 }
 
-func removeMacieMembers(svc *macie2.Macie2, accountIds []*string) {
+func removeMacieMembers(svc *macie2.Macie2, memberAccountIds []*string) error {
 
 	// Member accounts must first be disassociated
-	for _, accountId := range accountIds {
+	for _, accountId := range memberAccountIds {
 		_, err := svc.DisassociateMember(&macie2.DisassociateMemberInput{Id: accountId})
 		if err != nil {
-			if strings.Contains(err.Error(), "there isn't a delegated Macie administrator") {
-				logging.Logger.Infof("No delegated Macie administrator found.")
-			} else {
-				logging.Logger.Errorf("[Failed] Failed to disassociate members from macie")
-				telemetry.TrackEvent(commonTelemetry.EventContext{
-					EventName: "Error disassociating members from macie",
-				}, map[string]interface{}{
-					"region": *svc.Config.Region,
-					"reason": "Unable to disassociate",
-				})
-			}
-			return
+			return err
 		}
-		logging.Logger.Debugf("%s member account disassociated", accountId)
+		logging.Logger.Debugf("%s member account disassociated", *accountId)
 
 		// Once disassociated, member accounts can be deleted
 		_, err = svc.DeleteMember(&macie2.DeleteMemberInput{Id: accountId})
 		if err != nil {
-			logging.Logger.Errorf("[Failed] Failed to delete members from macie")
-			telemetry.TrackEvent(commonTelemetry.EventContext{
-				EventName: "Error deleting members from macie",
-			}, map[string]interface{}{
-				"region": *svc.Config.Region,
-				"reason": "Unable to delete",
-			})
-			return
+			return err
 		}
-		logging.Logger.Debugf("%s member account deleted", accountId)
+		logging.Logger.Debugf("%s member account deleted", *accountId)
 	}
+	return nil
 }
 
-func disassociateMacieAdministratorAccount(svc *macie2.Macie2) {
-
-	// Check for an associated admin account as it must be disassociated before disabling macie
-	adminAccount, err := svc.GetAdministratorAccount(&macie2.GetAdministratorAccountInput{})
-	if err != nil {
-		logging.Logger.Errorf("[Failed] Failed to check for administrator account")
-		telemetry.TrackEvent(commonTelemetry.EventContext{
-			EventName: "Error checking for administrator account in macie",
-		}, map[string]interface{}{
-			"region": *svc.Config.Region,
-			"reason": "Unable to find admin account",
-		})
-	}
-	// Disassociate if one exists
-	if adminAccount.Administrator != nil {
-		_, err := svc.DisassociateFromAdministratorAccount(&macie2.DisassociateFromAdministratorAccountInput{})
-		if err != nil {
-			logging.Logger.Errorf("[Failed] Failed to disassociate from administrator account")
-			telemetry.TrackEvent(commonTelemetry.EventContext{
-				EventName: "Error disassociating administrator account in macie",
-			}, map[string]interface{}{
-				"region": *svc.Config.Region,
-				"reason": "Unable to disassociate admin account",
-			})
-		} else {
-			logging.Logger.Debugf("Successfully disassociated from administrator account")
-		}
-	}
-}
-
-func nukeAllMacieMemberAccounts(session *session.Session, identifier []string) error {
+func nukeMacie(session *session.Session, identifier []string) error {
 	svc := macie2.New(session)
 	region := aws.StringValue(session.Config.Region)
 
@@ -163,12 +116,48 @@ func nukeAllMacieMemberAccounts(session *session.Session, identifier []string) e
 		})
 	}
 	if err == nil && len(memberAccountIds) > 0 {
-		removeMacieMembers(svc, memberAccountIds)
+		err = removeMacieMembers(svc, memberAccountIds)
+		if err != nil {
+			logging.Logger.Errorf("[Failed] Failed to remove members from macie")
+			telemetry.TrackEvent(commonTelemetry.EventContext{
+				EventName: "Error removing members from macie",
+			}, map[string]interface{}{
+				"region": *svc.Config.Region,
+				"reason": "Unable to remove members",
+			})
+		}
 	}
 
-	// Disassociate account from any administrator accounts
-	// Macie cannot be disable with an active administrator account
-	disassociateMacieAdministratorAccount(svc)
+	// Check for an administrator account
+	// Macie cannot be disabled with an active administrator account
+	adminAccount, err := svc.GetAdministratorAccount(&macie2.GetAdministratorAccountInput{})
+	if err != nil {
+		if strings.Contains(err.Error(), "there isn't a delegated Macie administrator") {
+			logging.Logger.Infof("No delegated Macie administrator found to remove.")
+		} else {
+			logging.Logger.Errorf("[Failed] Failed to check for administrator account")
+			telemetry.TrackEvent(commonTelemetry.EventContext{
+				EventName: "Error checking for administrator account in Macie",
+			}, map[string]interface{}{
+				"region": *svc.Config.Region,
+				"reason": "Unable to find admin account",
+			})
+		}
+	}
+
+	// Disassociate administrator account if it exists
+	if adminAccount.Administrator != nil {
+		_, err := svc.DisassociateFromAdministratorAccount(&macie2.DisassociateFromAdministratorAccountInput{})
+		if err != nil {
+			logging.Logger.Errorf("[Failed] Failed to disassociate from administrator account")
+			telemetry.TrackEvent(commonTelemetry.EventContext{
+				EventName: "Error disassociating administrator account in Macie",
+			}, map[string]interface{}{
+				"region": *svc.Config.Region,
+				"reason": "Unable to disassociate admin account",
+			})
+		}
+	}
 
 	// Disable Macie
 	_, err = svc.DisableMacie(&macie2.DisableMacieInput{})
@@ -196,51 +185,3 @@ func nukeAllMacieMemberAccounts(session *session.Session, identifier []string) e
 	}
 	return nil
 }
-
-// func nukeAllMacieMemberAccounts(session *session.Session, identifiers []string) error {
-// 	svc := macie2.New(session)
-// 	region := aws.StringValue(session.Config.Region)
-
-// 	if len(identifiers) == 0 {
-// 		logging.Logger.Debugf("No Macie member accounts to nuke in region %s", *session.Config.Region)
-// 		return nil
-// 	}
-
-// 	logging.Logger.Debugf("Deleting Macie account membership and disabling Macie in %s", region)
-
-// 	for _, accountId := range identifiers {
-// 		_, disassociateErr := svc.DisassociateFromAdministratorAccount(&macie2.DisassociateFromAdministratorAccountInput{})
-
-// 		if disassociateErr != nil {
-// 			telemetry.TrackEvent(commonTelemetry.EventContext{
-// 				EventName: "Error Nuking MACIE",
-// 			}, map[string]interface{}{
-// 				"region": *session.Config.Region,
-// 			})
-// 			return errors.WithStackTrace(disassociateErr)
-// 		}
-
-// 		_, err := svc.DisableMacie(&macie2.DisableMacieInput{})
-
-// 		// Record status of this resource
-// 		e := report.Entry{
-// 			Identifier:   accountId,
-// 			ResourceType: "Macie member account",
-// 			Error:        err,
-// 		}
-// 		report.Record(e)
-
-// 		if err != nil {
-// 			telemetry.TrackEvent(commonTelemetry.EventContext{
-// 				EventName: "Error Nuking MACIE",
-// 			}, map[string]interface{}{
-// 				"region": *session.Config.Region,
-// 			})
-// 			return errors.WithStackTrace(err)
-// 		}
-
-// 		logging.Logger.Debugf("[OK] Macie account association for accountId %s deleted in %s", accountId, region)
-// 	}
-
-// 	return nil
-// }
--- aws/macie_test.go
+++ aws/macie_test.go
@@ -1,46 +1,63 @@
 package aws
 
-//func TestListMacieAccounts(t *testing.T) {
-//	// Currently we hardcode to region us-east-1, because this is where our "standing" test invite exists
-//	region := "us-east-1"
-//	session, err := session.NewSession(&aws.Config{Region: aws.String(region)})
-//	require.NoError(t, err)
-//
-//	accountId, err := util.GetCurrentAccountId(session)
-//	require.NoError(t, err)
-//
-//	acceptTestInvite(t, session)
-//	// Clean up after test by deleting the macie account association
-//	defer nukeAllMacieMemberAccounts(session, []string{accountId})
-//
-//	retrievedAccountIds, lookupErr := getAllMacieMemberAccounts(session)
-//	require.NoError(t, lookupErr)
-//
-//	assert.Contains(t, retrievedAccountIds, accountId)
-//}
+import (
+	"strings"
+	"testing"
+	"time"
 
-// Macie is not very conducive to programmatic testing. In order to make this test work, we  maintain a standing invite
-// from our phxdevops test account to our nuclear-wasteland account. We can continuously "nuke" our membership because
-// Macie supports a member account *that was invited* to remove its own association at any time. Meanwhile, disassociating
-// in this manner does not destroy or invalidate the original invitation, which allows us to to continually re-accept it
-// from our nuclear-wasteland account (where cloud-nuke tests are run), just so that we can nuke it again
-//
-// Macie is also regional, so for the purposes of cost-savings and lower admin overhead, we're initially only testing this
-// in the one hardcoded region - us-east-1
-//
-// The other reason we only test in us-east-1 is to avoid conflict with our Macie test in the CIS service catalog, which uses
-// these same two accounts for similar purposes, but in EU regions.
-// See: https://github.com/gruntwork-io/terraform-aws-cis-service-catalog/blob/master/test/security/macie_test.go
-//func acceptTestInvite(t *testing.T, session *session.Session) {
-//	svc := macie2.New(session)
-//
-//	// Accept the "standing" invite from our other test account to become a Macie member account
-//	// This works because Macie invites don't expire or get deleted when you disassociate your member account following an invitation
-//	acceptInviteInput := &macie2.AcceptInvitationInput{
-//		AdministratorAccountId: aws.String("353720269506"),                     // sandbox
-//		InvitationId:           aws.String("18c0febb89142640f07ba497b19bac8e"), // "standing" test invite ID
-//	}
-//
-//	_, acceptInviteErr := svc.AcceptInvitation(acceptInviteInput)
-//	require.NoError(t, acceptInviteErr)
-//}
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/macie2"
+	"github.com/gruntwork-io/cloud-nuke/logging"
+	"github.com/gruntwork-io/cloud-nuke/telemetry"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+// Macie tests are limited to testing the ability to find and disable basic Macie
+// features. The functionality of cloud-nuke disassociating/deleting members and
+// disassociating administrator accounts requires the use of multiple AWS accounts and the
+// ability to send and accept invitations within those accounts.
+
+func TestMacie(t *testing.T) {
+	telemetry.InitTelemetry("cloud-nuke", "", "")
+	t.Parallel()
+
+	region, err := getRandomRegion()
+	require.NoError(t, err)
+	logging.Logger.Infof("Region: %s", region)
+
+	region = "us-east-1"
+
+	session, err := session.NewSession(&aws.Config{Region: aws.String(region)})
+	require.NoError(t, err)
+
+	svc := macie2.New(session)
+
+	// Check if Macie is enabled
+	_, err = svc.GetMacieSession(&macie2.GetMacieSessionInput{})
+
+	if err != nil {
+		// GetMacieSession throws an error if Macie is not enabled
+		if strings.Contains(err.Error(), "Macie is not enabled") {
+			logging.Logger.Infof("Macie not enabled.")
+			logging.Logger.Infof("Enabling Macie")
+			_, err := svc.EnableMacie(&macie2.EnableMacieInput{})
+			require.NoError(t, err)
+		} else {
+			require.NoError(t, err)
+		}
+	} else {
+		logging.Logger.Infof("Macie already enabled")
+	}
+
+	macieEnabled, err := getMacie(session, time.Now())
+	require.NoError(t, err)
+
+	logging.Logger.Infof("Nuking Macie")
+	nukeMacie(session, macieEnabled)
+
+	macieEnabled, err = getMacie(session, time.Now())
+	require.NoError(t, err)
+	assert.Empty(t, macieEnabled)
+}
