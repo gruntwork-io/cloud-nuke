c12a549d85a84d5dbeafc481abe6334d6ffb40a7
James Kwon <james@gruntwork.io>
2023-07-26 17:43:01 -0400

    refactor redshift resource type

--- aws/redshift.go
+++ aws/redshift.go
@@ -2,7 +2,6 @@ package aws
 
 import (
 	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/redshift"
 	"github.com/gruntwork-io/cloud-nuke/config"
 	"github.com/gruntwork-io/cloud-nuke/logging"
@@ -10,55 +9,46 @@ import (
 	"github.com/gruntwork-io/cloud-nuke/telemetry"
 	"github.com/gruntwork-io/go-commons/errors"
 	commonTelemetry "github.com/gruntwork-io/go-commons/telemetry"
-	"time"
 )
 
-func getAllRedshiftClusters(session *session.Session, region string, excludeAfter time.Time, configObj config.Config) ([]*string, error) {
-	svc := redshift.New(session)
+func (rc RedshiftClusters) getAll(configObj config.Config) ([]*string, error) {
 	var clusterIds []*string
-	err := svc.DescribeClustersPages(
+	err := rc.Client.DescribeClustersPages(
 		&redshift.DescribeClustersInput{},
 		func(page *redshift.DescribeClustersOutput, lastPage bool) bool {
 			for _, cluster := range page.Clusters {
-				if shouldIncludeRedshiftCluster(cluster, excludeAfter, configObj) {
+				if configObj.Redshift.ShouldInclude(config.ResourceValue{
+					Time: cluster.ClusterCreateTime,
+					Name: cluster.ClusterIdentifier,
+				}) {
 					clusterIds = append(clusterIds, cluster.ClusterIdentifier)
 				}
 			}
+
 			return !lastPage
 		},
 	)
+
 	return clusterIds, errors.WithStackTrace(err)
 }
 
-func shouldIncludeRedshiftCluster(cluster *redshift.Cluster, excludeAfter time.Time, configObj config.Config) bool {
-	if cluster == nil {
-		return false
-	}
-	if excludeAfter.Before(*cluster.ClusterCreateTime) {
-		return false
-	}
-	return config.ShouldInclude(
-		aws.StringValue(cluster.ClusterIdentifier),
-		configObj.Redshift.IncludeRule.NamesRegExp,
-		configObj.Redshift.ExcludeRule.NamesRegExp,
-	)
-}
-
-func nukeAllRedshiftClusters(session *session.Session, identifiers []*string) error {
-	svc := redshift.New(session)
+func (rc RedshiftClusters) nukeAll(identifiers []*string) error {
 	if len(identifiers) == 0 {
-		logging.Logger.Debugf("No Redshift Clusters to nuke in region %s", *session.Config.Region)
+		logging.Logger.Debugf("No Redshift Clusters to nuke in region %s", rc.Region)
 		return nil
 	}
-	logging.Logger.Debugf("Deleting all Redshift Clusters in region %s", *session.Config.Region)
+	logging.Logger.Debugf("Deleting all Redshift Clusters in region %s", rc.Region)
 	deletedIds := []*string{}
 	for _, id := range identifiers {
-		_, err := svc.DeleteCluster(&redshift.DeleteClusterInput{ClusterIdentifier: id, SkipFinalClusterSnapshot: aws.Bool(true)})
+		_, err := rc.Client.DeleteCluster(&redshift.DeleteClusterInput{
+			ClusterIdentifier:        id,
+			SkipFinalClusterSnapshot: aws.Bool(true),
+		})
 		if err != nil {
 			telemetry.TrackEvent(commonTelemetry.EventContext{
 				EventName: "Error Nuking RedshiftCluster",
 			}, map[string]interface{}{
-				"region": *session.Config.Region,
+				"region": rc.Region,
 			})
 			logging.Logger.Errorf("[Failed] %s: %s", *id, err)
 		} else {
@@ -69,7 +59,7 @@ func nukeAllRedshiftClusters(session *session.Session, identifiers []*string) er
 
 	if len(deletedIds) > 0 {
 		for _, id := range deletedIds {
-			err := svc.WaitUntilClusterDeleted(&redshift.DescribeClustersInput{ClusterIdentifier: id})
+			err := rc.Client.WaitUntilClusterDeleted(&redshift.DescribeClustersInput{ClusterIdentifier: id})
 			// Record status of this resource
 			e := report.Entry{
 				Identifier:   aws.StringValue(id),
@@ -81,13 +71,13 @@ func nukeAllRedshiftClusters(session *session.Session, identifiers []*string) er
 				telemetry.TrackEvent(commonTelemetry.EventContext{
 					EventName: "Error Nuking Redshift Cluster",
 				}, map[string]interface{}{
-					"region": *session.Config.Region,
+					"region": rc.Region,
 				})
 				logging.Logger.Errorf("[Failed] %s", err)
 				return errors.WithStackTrace(err)
 			}
 		}
 	}
-	logging.Logger.Debugf("[OK] %d Redshift Cluster(s) deleted in %s", len(deletedIds), *session.Config.Region)
+	logging.Logger.Debugf("[OK] %d Redshift Cluster(s) deleted in %s", len(deletedIds), rc.Region)
 	return nil
 }
