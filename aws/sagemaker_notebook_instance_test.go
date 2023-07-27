ff1a1574f18b1283bf1bbfa7936713713ceb739f
James Kwon <96548424+hongil0316@users.noreply.github.com>
2023-07-27 11:53:45 -0400

    refactor sagemaker resource type (#519)

--- aws/sagemaker_notebook_instance_test.go
+++ aws/sagemaker_notebook_instance_test.go
@@ -1,104 +1,121 @@
 package aws
 
 import (
+	"github.com/aws/aws-sdk-go-v2/aws"
+	awsgo "github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/sagemaker"
+	"github.com/aws/aws-sdk-go/service/sagemaker/sagemakeriface"
+	"github.com/gruntwork-io/cloud-nuke/config"
 	"github.com/gruntwork-io/cloud-nuke/telemetry"
-	"strings"
+	"github.com/stretchr/testify/require"
+	"regexp"
 	"testing"
 	"time"
-
-	awsgo "github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/session"
-	"github.com/aws/aws-sdk-go/service/sagemaker"
-	"github.com/gruntwork-io/cloud-nuke/config"
-	"github.com/gruntwork-io/cloud-nuke/logging"
-	"github.com/gruntwork-io/cloud-nuke/util"
-	"github.com/gruntwork-io/go-commons/errors"
-	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
 )
 
 // There's a built-in function WaitUntilDBInstanceAvailable but
 // the times that it was tested, it wasn't returning anything so we'll leave with the
 // custom one.
 
-func waitUntilNotebookInstanceCreated(svc *sagemaker.SageMaker, name *string) error {
-	input := &sagemaker.DescribeNotebookInstanceInput{
-		NotebookInstanceName: name,
-	}
-
-	for i := 0; i < 600; i++ {
-		instance, err := svc.DescribeNotebookInstance(input)
-		status := instance.NotebookInstanceStatus
-
-		if awsgo.StringValue(status) != "Pending" {
-			return nil
-		}
-
-		if err != nil {
-			return err
-		}
-
-		time.Sleep(1 * time.Second)
-		logging.Logger.Debug("Waiting for SageMaker Notebook Instance to be created")
-	}
-
-	return SageMakerNotebookInstanceDeleteError{name: *name}
+type mockedSageMakerNotebookInstance struct {
+	sagemakeriface.SageMakerAPI
+	ListNotebookInstancesOutput  sagemaker.ListNotebookInstancesOutput
+	StopNotebookInstanceOutput   sagemaker.StopNotebookInstanceOutput
+	DeleteNotebookInstanceOutput sagemaker.DeleteNotebookInstanceOutput
 }
 
-func createTestNotebookInstance(t *testing.T, session *session.Session, name string, roleArn string) {
-	svc := sagemaker.New(session)
-
-	params := &sagemaker.CreateNotebookInstanceInput{
-		InstanceType:         awsgo.String("ml.t2.medium"),
-		NotebookInstanceName: awsgo.String(name),
-		RoleArn:              awsgo.String(roleArn),
-	}
-
-	_, err := svc.CreateNotebookInstance(params)
-	require.NoError(t, err)
-
-	waitUntilNotebookInstanceCreated(svc, &name)
+func (m mockedSageMakerNotebookInstance) ListNotebookInstances(input *sagemaker.ListNotebookInstancesInput) (*sagemaker.ListNotebookInstancesOutput, error) {
+	return &m.ListNotebookInstancesOutput, nil
 }
 
-func TestNukeNotebookInstance(t *testing.T) {
+func (m mockedSageMakerNotebookInstance) StopNotebookInstance(input *sagemaker.StopNotebookInstanceInput) (*sagemaker.StopNotebookInstanceOutput, error) {
+	return &m.StopNotebookInstanceOutput, nil
+}
+
+func (m mockedSageMakerNotebookInstance) WaitUntilNotebookInstanceStopped(*sagemaker.DescribeNotebookInstanceInput) error {
+	return nil
+}
+
+func (m mockedSageMakerNotebookInstance) WaitUntilNotebookInstanceDeleted(*sagemaker.DescribeNotebookInstanceInput) error {
+	return nil
+}
+
+func (m mockedSageMakerNotebookInstance) DeleteNotebookInstance(input *sagemaker.DeleteNotebookInstanceInput) (*sagemaker.DeleteNotebookInstanceOutput, error) {
+	return &m.DeleteNotebookInstanceOutput, nil
+}
+
+func TestSageMakerNotebookInstances_GetAll(t *testing.T) {
 	telemetry.InitTelemetry("cloud-nuke", "")
 	t.Parallel()
 
-	region, err := getRandomRegion()
-
-	require.NoError(t, errors.WithStackTrace(err))
-
-	session, err := session.NewSessionWithOptions(
-		session.Options{
-			SharedConfigState: session.SharedConfigEnable,
-			Config: awsgo.Config{
-				Region: awsgo.String(region),
+	now := time.Now()
+	testName1 := "test1"
+	testName2 := "test2"
+	smni := SageMakerNotebookInstances{
+		Client: mockedSageMakerNotebookInstance{
+			ListNotebookInstancesOutput: sagemaker.ListNotebookInstancesOutput{
+				NotebookInstances: []*sagemaker.NotebookInstanceSummary{
+					{
+						NotebookInstanceName: awsgo.String(testName1),
+						CreationTime:         awsgo.Time(now),
+					},
+					{
+						NotebookInstanceName: awsgo.String(testName2),
+						CreationTime:         awsgo.Time(now.Add(1)),
+					},
+				},
 			},
 		},
-	)
-
-	notebookName := "cloud-nuke-test-" + util.UniqueID()
-	excludeAfter := time.Now().Add(1 * time.Hour)
-
-	role := createNotebookRole(t, session, notebookName+"-role")
-	defer deleteNotebookRole(session, role)
-
-	createTestNotebookInstance(t, session, notebookName, *role.Arn)
-
-	defer func() {
-		nukeAllNotebookInstances(session, []*string{&notebookName})
-
-		notebookNames, _ := getAllNotebookInstances(session, excludeAfter, config.Config{})
-
-		assert.NotContains(t, awsgo.StringValueSlice(notebookNames), strings.ToLower(notebookName))
-	}()
-
-	instances, err := getAllNotebookInstances(session, excludeAfter, config.Config{})
-
-	if err != nil {
-		assert.Failf(t, "Unable to fetch list of SageMaker Notebook Instances", errors.WithStackTrace(err).Error())
 	}
 
-	assert.Contains(t, awsgo.StringValueSlice(instances), notebookName)
+	tests := map[string]struct {
+		configObj config.ResourceType
+		expected  []string
+	}{
+		"emptyFilter": {
+			configObj: config.ResourceType{},
+			expected:  []string{testName1, testName2},
+		},
+		"nameExclusionFilter": {
+			configObj: config.ResourceType{
+				ExcludeRule: config.FilterRule{
+					NamesRegExp: []config.Expression{{
+						RE: *regexp.MustCompile(testName1),
+					}}},
+			},
+			expected: []string{testName2},
+		},
+		"timeAfterExclusionFilter": {
+			configObj: config.ResourceType{
+				ExcludeRule: config.FilterRule{
+					TimeAfter: aws.Time(now.Add(-1 * time.Hour)),
+				}},
+			expected: []string{},
+		},
+	}
+	for name, tc := range tests {
+		t.Run(name, func(t *testing.T) {
+			names, err := smni.getAll(config.Config{
+				SageMakerNotebook: tc.configObj,
+			})
+			require.NoError(t, err)
+			require.Equal(t, tc.expected, awsgo.StringValueSlice(names))
+		})
+	}
 
+}
+
+func TestSageMakerNotebookInstances_NukeAll(t *testing.T) {
+	telemetry.InitTelemetry("cloud-nuke", "")
+	t.Parallel()
+
+	smni := SageMakerNotebookInstances{
+		Client: mockedSageMakerNotebookInstance{
+			StopNotebookInstanceOutput:   sagemaker.StopNotebookInstanceOutput{},
+			DeleteNotebookInstanceOutput: sagemaker.DeleteNotebookInstanceOutput{},
+		},
+	}
+
+	err := smni.nukeAll([]*string{aws.String("test")})
+	require.NoError(t, err)
 }
