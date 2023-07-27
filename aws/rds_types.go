293e7ab227a3a00766a0015f569fb82fbe5c8bd9
James Kwon <96548424+hongil0316@users.noreply.github.com>
2023-07-27 11:54:06 -0400

    Refactor DBINstnaces Resource Type (#521)

--- aws/rds_types.go
+++ aws/rds_types.go
@@ -13,23 +13,23 @@ type DBInstances struct {
 	InstanceNames []string
 }
 
-func (instance DBInstances) ResourceName() string {
+func (di DBInstances) ResourceName() string {
 	return "rds"
 }
 
 // ResourceIdentifiers - The instance names of the rds db instances
-func (instance DBInstances) ResourceIdentifiers() []string {
-	return instance.InstanceNames
+func (di DBInstances) ResourceIdentifiers() []string {
+	return di.InstanceNames
 }
 
-func (instance DBInstances) MaxBatchSize() int {
+func (di DBInstances) MaxBatchSize() int {
 	// Tentative batch size to ensure AWS doesn't throttle
 	return 49
 }
 
 // Nuke - nuke 'em all!!!
-func (instance DBInstances) Nuke(session *session.Session, identifiers []string) error {
-	if err := nukeAllRdsInstances(session, awsgo.StringSlice(identifiers)); err != nil {
+func (di DBInstances) Nuke(session *session.Session, identifiers []string) error {
+	if err := di.nukeAll(awsgo.StringSlice(identifiers)); err != nil {
 		return errors.WithStackTrace(err)
 	}
 
