185933603d8587eddaf185e02f8e864377a5c66b
James Kwon <96548424+hongil0316@users.noreply.github.com>
2023-08-02 15:35:33 -0400

    refactor guard duty (#538)

--- aws/guardduty_test.go
+++ aws/guardduty_test.go
@@ -1,178 +1,95 @@
 package aws
 
 import (
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/guardduty"
+	"github.com/aws/aws-sdk-go/service/guardduty/guarddutyiface"
+	"github.com/gruntwork-io/cloud-nuke/config"
 	"github.com/gruntwork-io/cloud-nuke/telemetry"
+	"github.com/stretchr/testify/require"
 	"testing"
 	"time"
-
-	"github.com/aws/aws-sdk-go/aws"
-	awsgo "github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/session"
-	"github.com/aws/aws-sdk-go/service/guardduty"
-	"github.com/gruntwork-io/cloud-nuke/config"
-	"github.com/gruntwork-io/go-commons/errors"
-	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
 )
 
-func createTestGuardDutyDetector(t *testing.T, session *session.Session) string {
-	svc := guardduty.New(session)
+type mockedGuardDuty struct {
+	guarddutyiface.GuardDutyAPI
+	ListDetectorsPagesOutput guardduty.ListDetectorsOutput
+	GetDetectorOutput        guardduty.GetDetectorOutput
+	DeleteDetectorOutput     guardduty.DeleteDetectorOutput
+}
 
-	param := &guardduty.CreateDetectorInput{
-		ClientToken: aws.String("test-value-idempotent-token"),
-		DataSources: &guardduty.DataSourceConfigurations{
-			Kubernetes: &guardduty.KubernetesConfiguration{
-				AuditLogs: &guardduty.KubernetesAuditLogsConfiguration{
-					Enable: aws.Bool(false),
+func (m mockedGuardDuty) ListDetectorsPages(input *guardduty.ListDetectorsInput, callback func(*guardduty.ListDetectorsOutput, bool) bool) error {
+	callback(&m.ListDetectorsPagesOutput, true)
+	return nil
+}
+
+func (m mockedGuardDuty) GetDetector(input *guardduty.GetDetectorInput) (*guardduty.GetDetectorOutput, error) {
+	return &m.GetDetectorOutput, nil
+}
+
+func (m mockedGuardDuty) DeleteDetector(input *guardduty.DeleteDetectorInput) (*guardduty.DeleteDetectorOutput, error) {
+	return &m.DeleteDetectorOutput, nil
+}
+
+func TestGuardDuty_GetAll(t *testing.T) {
+	telemetry.InitTelemetry("cloud-nuke", "")
+	t.Parallel()
+
+	testId1 := "test-detector-id-1"
+	testId2 := "test-detector-id-2"
+	now := time.Now()
+	gd := GuardDuty{
+		Client: mockedGuardDuty{
+			ListDetectorsPagesOutput: guardduty.ListDetectorsOutput{
+				DetectorIds: []*string{
+					aws.String(testId1),
+					aws.String(testId2),
 				},
 			},
-			S3Logs: &guardduty.S3LogsConfiguration{
-				Enable: aws.Bool(false),
+			GetDetectorOutput: guardduty.GetDetectorOutput{
+				CreatedAt: aws.String(now.Format(time.RFC3339)),
 			},
 		},
-		Enable:                     awsgo.Bool(true),
-		FindingPublishingFrequency: nil,
-		Tags: map[string]*string{
-			"test-detector": aws.String("true"),
+	}
+
+	tests := map[string]struct {
+		configObj config.ResourceType
+		expected  []string
+	}{
+		"emptyFilter": {
+			configObj: config.ResourceType{},
+			expected:  []string{testId1, testId2},
+		},
+		"timeAfterExclusionFilter": {
+			configObj: config.ResourceType{
+				ExcludeRule: config.FilterRule{
+					TimeAfter: aws.Time(now.Add(-2 * time.Hour)),
+				}},
+			expected: []string{},
+		},
+	}
+	for name, tc := range tests {
+		t.Run(name, func(t *testing.T) {
+			names, err := gd.getAll(config.Config{
+				GuardDuty: tc.configObj,
+			})
+			require.NoError(t, err)
+			require.Equal(t, tc.expected, aws.StringValueSlice(names))
+		})
+	}
+}
+
+func TestGuardDuty_NukeAll(t *testing.T) {
+	telemetry.InitTelemetry("cloud-nuke", "")
+	t.Parallel()
+
+	gd := GuardDuty{
+		Client: mockedGuardDuty{
+			DeleteDetectorOutput: guardduty.DeleteDetectorOutput{},
 		},
 	}
 
-	detectorOutput, err := svc.CreateDetector(param)
-	if err != nil {
-		assert.Failf(t, "Could not create test GuardDuty Detector", errors.WithStackTrace(err).Error())
-	}
-
-	return awsgo.StringValue(detectorOutput.DetectorId)
-}
-
-func TestListGuardDutyDetectors(t *testing.T) {
-	telemetry.InitTelemetry("cloud-nuke", "")
-	t.Parallel()
-
-	region, err := getRandomRegion()
-	if err != nil {
-		assert.Fail(t, errors.WithStackTrace(err).Error())
-	}
-
-	session, err := session.NewSession(&awsgo.Config{
-		Region: awsgo.String(region),
-	},
-	)
-	if err != nil {
-		assert.Fail(t, errors.WithStackTrace(err).Error())
-	}
-
-	testDetectorId := createTestGuardDutyDetector(t, session)
-
-	// clean up after this test
-	defer nukeAllGuardDutyDetectors(session, []string{testDetectorId})
-
-	detectorIds, lookupErr := getAllGuardDutyDetectors(session, time.Now(), config.Config{}, GuardDuty{}.MaxBatchSize())
-
-	require.NoError(t, lookupErr)
-
-	if err != nil {
-		assert.Fail(t, "Unable to fetch list of GuardDuty Detector Ids")
-	}
-
-	assert.Contains(t, detectorIds, testDetectorId)
-}
-
-func TestTimeFilterExclusionNewlyCreatedGuardDutyDetector(t *testing.T) {
-	telemetry.InitTelemetry("cloud-nuke", "")
-	t.Parallel()
-
-	region, err := getRandomRegion()
+	err := gd.nukeAll([]string{"test"})
 	require.NoError(t, err)
 
-	session, err := session.NewSession(&aws.Config{Region: aws.String(region)})
-	require.NoError(t, err)
-
-	testDetectorId := createTestGuardDutyDetector(t, session)
-	// Clean up after this test
-	defer nukeAllGuardDutyDetectors(session, []string{testDetectorId})
-
-	// Assert detectors are picked up without filters
-	detectorIds, err := getAllGuardDutyDetectors(session, time.Now(), config.Config{}, GuardDuty{}.MaxBatchSize())
-	require.NoError(t, err)
-	assert.Contains(t, detectorIds, testDetectorId)
-
-	// Assert detector doesn't appear when we look at detectors older than 1 Hour
-	olderThan := time.Now().Add(-1 * time.Hour)
-	detectorIdsOlder, err := getAllGuardDutyDetectors(session, olderThan, config.Config{}, GuardDuty{}.MaxBatchSize())
-	require.NoError(t, err)
-	assert.NotContains(t, detectorIdsOlder, testDetectorId)
-}
-
-func TestNukeGuardDutyDetectorOne(t *testing.T) {
-	telemetry.InitTelemetry("cloud-nuke", "")
-	t.Parallel()
-
-	region, err := getRandomRegion()
-	require.NoError(t, err)
-
-	session, err := session.NewSession(&aws.Config{Region: aws.String(region)})
-	require.NoError(t, err)
-
-	testDetectorId := createTestGuardDutyDetector(t, session)
-
-	// Make sure the GuardDuty test detector was created and can be listed
-	detectorIdsPreNuke, lookupErr := getAllGuardDutyDetectors(session, time.Now(), config.Config{}, GuardDuty{}.MaxBatchSize())
-	require.NoError(t, lookupErr)
-
-	require.Equal(t, 1, len(detectorIdsPreNuke))
-
-	identifiers := []string{testDetectorId}
-
-	require.NoError(
-		t,
-		nukeAllGuardDutyDetectors(session, identifiers),
-	)
-
-	// Make sure the GuardDuty detector was deleted
-	detectorIdsPostNuke, secondLookupErr := getAllGuardDutyDetectors(session, time.Now(), config.Config{}, GuardDuty{}.MaxBatchSize())
-	require.NoError(t, secondLookupErr)
-	require.Equal(t, 0, len(detectorIdsPostNuke))
-}
-
-// TestNukeGuardDutyDetectorMoreThanOne verifies that you can create and nuke multiple detectors in different regions simultaneously
-func TestNukeGuardDutyDetectorMoreThanOne(t *testing.T) {
-	telemetry.InitTelemetry("cloud-nuke", "")
-	t.Parallel()
-
-	region1, err := getRandomRegion()
-	require.NoError(t, err)
-
-	session1, createSessionErr := session.NewSession(&aws.Config{Region: aws.String(region1)})
-	require.NoError(t, createSessionErr)
-
-	testDetectorId1 := createTestGuardDutyDetector(t, session1)
-
-	region2, err := getRandomRegionWithExclusions([]string{region1})
-	require.NoError(t, err)
-
-	session2, createSessionErr2 := session.NewSession(&aws.Config{Region: aws.String(region2)})
-	require.NoError(t, createSessionErr2)
-
-	testDetectorId2 := createTestGuardDutyDetector(t, session2)
-
-	require.NoError(
-		t,
-		nukeAllGuardDutyDetectors(session1, []string{testDetectorId1}),
-	)
-
-	require.NoError(
-		t,
-		nukeAllGuardDutyDetectors(session2, []string{testDetectorId2}),
-	)
-
-	// Make sure the GuardDuty detector was deleted
-	detectorIdsPostNuke1, lookupErr := getAllGuardDutyDetectors(session1, time.Now(), config.Config{}, GuardDuty{}.MaxBatchSize())
-	require.NoError(t, lookupErr)
-	require.Equal(t, 0, len(detectorIdsPostNuke1))
-
-	// Make sure the GuardDuty detector was deleted
-	detectorIdsPostNuke2, lookupErr := getAllGuardDutyDetectors(session2, time.Now(), config.Config{}, GuardDuty{}.MaxBatchSize())
-	require.NoError(t, lookupErr)
-	require.Equal(t, 0, len(detectorIdsPostNuke2))
 }
