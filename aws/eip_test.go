bbaa2b78064caf85544591740e8e2955ff6175af
James Kwon <96548424+hongil0316@users.noreply.github.com>
2023-08-09 17:07:05 -0400

    Refactor Eip (#545)
    * refactor EIP
    
    * rebase fix

--- aws/eip_test.go
+++ aws/eip_test.go
@@ -1,295 +1,124 @@
 package aws
 
 import (
+	"github.com/aws/aws-sdk-go-v2/aws"
+	awsgo "github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/ec2"
+	"github.com/aws/aws-sdk-go/service/ec2/ec2iface"
+	"github.com/gruntwork-io/cloud-nuke/config"
 	"github.com/gruntwork-io/cloud-nuke/telemetry"
+	"github.com/gruntwork-io/cloud-nuke/util"
+	"github.com/stretchr/testify/require"
 	"regexp"
-	"sync"
 	"testing"
 	"time"
-
-	awsgo "github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/session"
-	"github.com/aws/aws-sdk-go/service/ec2"
-	"github.com/gruntwork-io/cloud-nuke/config"
-	"github.com/gruntwork-io/cloud-nuke/logging"
-	"github.com/gruntwork-io/go-commons/errors"
-	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
 )
 
-// eipLock - lock to allow single call to AllocateAddress, parallel calls may generate same address id
-var eipLock = sync.Mutex{}
-
-func createTestEIPAddress(t *testing.T, session *session.Session) ec2.Address {
-	eipLock.Lock()
-	defer eipLock.Unlock()
-	svc := ec2.New(session)
-	result, err := svc.AllocateAddress(&ec2.AllocateAddressInput{})
-	if err != nil {
-		assert.Fail(t, errors.WithStackTrace(err).Error())
-	}
-
-	output, err := svc.DescribeAddresses(&ec2.DescribeAddressesInput{
-		AllocationIds: []*string{result.AllocationId},
-	})
-
-	if err != nil {
-		assert.Fail(t, errors.WithStackTrace(err).Error())
-	}
-
-	return *output.Addresses[0]
+type mockedEIPAddresses struct {
+	ec2iface.EC2API
+	DescribeAddressesOutput ec2.DescribeAddressesOutput
+	ReleaseAddressOutput    ec2.ReleaseAddressOutput
 }
 
-func TestSetFirstSeenTag(t *testing.T) {
+func (m mockedEIPAddresses) DescribeAddresses(*ec2.DescribeAddressesInput) (*ec2.DescribeAddressesOutput, error) {
+	return &m.DescribeAddressesOutput, nil
+}
+
+func (m mockedEIPAddresses) ReleaseAddress(*ec2.ReleaseAddressInput) (*ec2.ReleaseAddressOutput, error) {
+	return &m.ReleaseAddressOutput, nil
+}
+
+func TestEIPAddress_GetAll(t *testing.T) {
 	telemetry.InitTelemetry("cloud-nuke", "")
 	t.Parallel()
 
-	const key = "cloud-nuke-first-seen"
-	const layout = "2006-01-02 15:04:05"
-
-	region, err := getRandomRegion()
-	if err != nil {
-		assert.Fail(t, errors.WithStackTrace(err).Error())
-	}
-
-	session, err := session.NewSession(&awsgo.Config{
-		Region: awsgo.String(region)},
-	)
-
-	if err != nil {
-		assert.Fail(t, errors.WithStackTrace(err).Error())
-	}
-
-	svc := ec2.New(session)
-	address := createTestEIPAddress(t, session)
-	now := time.Now().UTC()
-
-	// clean up after this test
-	defer nukeAllEIPAddresses(session, []*string{address.AllocationId})
-
-	if err := setFirstSeenTag(svc, address, key, now, layout); err != nil {
-		assert.Fail(t, errors.WithStackTrace(err).Error())
-	}
-
-	result, err := svc.DescribeTags(&ec2.DescribeTagsInput{
-		Filters: []*ec2.Filter{
-			{
-				Name:   awsgo.String("resource-id"),
-				Values: []*string{address.AllocationId},
-			},
-		},
-	})
-
-	assert.Len(t, result.Tags, 1)
-	assert.Equal(t, key, *result.Tags[0].Key)
-	assert.Equal(t, now.Format(layout), *result.Tags[0].Value)
-}
-
-func TestGetFirstSeenTag(t *testing.T) {
-	telemetry.InitTelemetry("cloud-nuke", "")
-	t.Parallel()
-
-	const key = "cloud-nuke-first-seen"
-	const layout = "2006-01-02 15:04:05"
-
-	region, err := getRandomRegion()
-	if err != nil {
-		assert.Fail(t, errors.WithStackTrace(err).Error())
-	}
-
-	session, err := session.NewSession(&awsgo.Config{
-		Region: awsgo.String(region)},
-	)
-
-	if err != nil {
-		assert.Fail(t, errors.WithStackTrace(err).Error())
-	}
-
-	svc := ec2.New(session)
-	address := createTestEIPAddress(t, session)
-	now := time.Now().UTC()
-
-	// clean up after this test
-	defer nukeAllEIPAddresses(session, []*string{address.AllocationId})
-
-	_, err = svc.CreateTags(&ec2.CreateTagsInput{
-		Resources: []*string{address.AllocationId},
-		Tags: []*ec2.Tag{
-			{
-				Key:   awsgo.String(key),
-				Value: awsgo.String(now.Format(layout)),
-			},
-		},
-	})
-
-	if err != nil {
-		assert.Fail(t, errors.WithStackTrace(err).Error())
-	}
-
-	result, err := svc.DescribeAddresses(&ec2.DescribeAddressesInput{
-		AllocationIds: []*string{address.AllocationId},
-	})
-
-	if err != nil {
-		assert.Fail(t, errors.WithStackTrace(err).Error())
-	}
-
-	firstSeenTime, err := getFirstSeenTag(svc, *result.Addresses[0], key, layout)
-	if err != nil {
-		assert.Fail(t, errors.WithStackTrace(err).Error())
-	}
-
-	assert.Equal(t, now.Format(layout), (*firstSeenTime).Format(layout))
-}
-
-func TestListEIPAddress(t *testing.T) {
-	telemetry.InitTelemetry("cloud-nuke", "")
-	t.Parallel()
-
-	region, err := getRandomRegion()
-	if err != nil {
-		assert.Fail(t, errors.WithStackTrace(err).Error())
-	}
-
-	session, err := session.NewSession(&awsgo.Config{
-		Region: awsgo.String(region)},
-	)
-
-	if err != nil {
-		assert.Fail(t, errors.WithStackTrace(err).Error())
-	}
-
-	address := createTestEIPAddress(t, session)
-	// clean up after this test
-	defer nukeAllEIPAddresses(session, []*string{address.AllocationId})
-
-	allocationIds, err := getAllEIPAddresses(session, region, time.Now().Add(1*time.Hour*-1), config.Config{})
-	require.NoError(t, err)
-
-	assert.NotContains(t, awsgo.StringValueSlice(allocationIds), awsgo.StringValue(address.AllocationId))
-
-	allocationIds, err = getAllEIPAddresses(session, region, time.Now().Add(1*time.Hour), config.Config{})
-	if err != nil {
-		assert.Fail(t, "Unable to fetch list of EIP Addresses")
-	}
-
-	assert.Contains(t, awsgo.StringValueSlice(allocationIds), awsgo.StringValue(address.AllocationId))
-}
-
-func TestNukeEIPAddress(t *testing.T) {
-	telemetry.InitTelemetry("cloud-nuke", "")
-	t.Parallel()
-
-	region, err := getRandomRegion()
-	if err != nil {
-		assert.Fail(t, errors.WithStackTrace(err).Error())
-	}
-
-	session, err := session.NewSession(&awsgo.Config{
-		Region: awsgo.String(region)},
-	)
-
-	if err != nil {
-		assert.Fail(t, errors.WithStackTrace(err).Error())
-	}
-
-	address := createTestEIPAddress(t, session)
-	if err := nukeAllEIPAddresses(session, []*string{address.AllocationId}); err != nil {
-		assert.Fail(t, errors.WithStackTrace(err).Error())
-	}
-
-	allocationIds, err := getAllEIPAddresses(session, region, time.Now().Add(1*time.Hour), config.Config{})
-	if err != nil {
-		assert.Fail(t, "Unable to fetch list of EIP Addresses")
-	}
-
-	assert.NotContains(t, awsgo.StringValueSlice(allocationIds), awsgo.StringValue(address.AllocationId))
-}
-
-// Test config file filtering works as expected
-func TestShouldIncludeElasticIP(t *testing.T) {
-	telemetry.InitTelemetry("cloud-nuke", "")
-
-	mockAddress := &ec2.Address{
-		Tags: []*ec2.Tag{
-			{
-				Key:   awsgo.String("Name"),
-				Value: awsgo.String("cloud-nuke-test"),
-			},
-			{
-				Key:   awsgo.String("Foo"),
-				Value: awsgo.String("Bar"),
-			},
-		},
-	}
-
-	mockExpression, err := regexp.Compile("^cloud-nuke-*")
-	if err != nil {
-		logging.Logger.Fatalf("There was an error compiling regex expression %v", err)
-	}
-
-	mockExcludeConfig := config.Config{
-		ElasticIP: config.ResourceType{
-			ExcludeRule: config.FilterRule{
-				NamesRegExp: []config.Expression{
+	now := time.Now()
+	testName1 := "test-eip1"
+	testAllocId1 := "alloc1"
+	testName2 := "test-eip2"
+	testAllocId2 := "alloc2"
+	ea := EIPAddresses{
+		Client: &mockedEIPAddresses{
+			DescribeAddressesOutput: ec2.DescribeAddressesOutput{
+				Addresses: []*ec2.Address{
 					{
-						RE: *mockExpression,
+						AllocationId: awsgo.String(testAllocId1),
+						Tags: []*ec2.Tag{
+							{
+								Key:   awsgo.String("Name"),
+								Value: awsgo.String(testName1),
+							},
+							{
+								Key:   awsgo.String(util.FirstSeenTagKey),
+								Value: awsgo.String(util.FormatTimestampTag(now)),
+							},
+						},
+					},
+					{
+						AllocationId: awsgo.String(testAllocId2),
+						Tags: []*ec2.Tag{
+							{
+								Key:   awsgo.String("Name"),
+								Value: awsgo.String(testName2),
+							},
+							{
+								Key:   awsgo.String(util.FirstSeenTagKey),
+								Value: awsgo.String(util.FormatTimestampTag(now.Add(1))),
+							},
+						},
 					},
 				},
 			},
 		},
 	}
 
-	mockIncludeConfig := config.Config{
-		ElasticIP: config.ResourceType{
-			IncludeRule: config.FilterRule{
-				NamesRegExp: []config.Expression{
-					{
-						RE: *mockExpression,
-					},
-				},
-			},
-		},
-	}
-
-	cases := []struct {
-		Name          string
-		Address       *ec2.Address
-		Config        config.Config
-		ExcludeAfter  time.Time
-		FirstSeenTime time.Time
-		Expected      bool
+	tests := map[string]struct {
+		configObj config.ResourceType
+		expected  []string
 	}{
-		{
-			Name:          "ConfigExclude",
-			Address:       mockAddress,
-			Config:        mockExcludeConfig,
-			ExcludeAfter:  time.Now().Add(1 * time.Hour),
-			FirstSeenTime: time.Now(),
-			Expected:      false,
+		"emptyFilter": {
+			configObj: config.ResourceType{},
+			expected:  []string{testAllocId1, testAllocId2},
 		},
-		{
-			Name:          "ConfigInclude",
-			Address:       mockAddress,
-			Config:        mockIncludeConfig,
-			ExcludeAfter:  time.Now().Add(1 * time.Hour),
-			FirstSeenTime: time.Now(),
-			Expected:      true,
+		"nameExclusionFilter": {
+			configObj: config.ResourceType{
+				ExcludeRule: config.FilterRule{
+					NamesRegExp: []config.Expression{{
+						RE: *regexp.MustCompile(testName1),
+					}}},
+			},
+			expected: []string{testAllocId2},
 		},
-		{
-			Name:          "NotOlderThan",
-			Address:       mockAddress,
-			Config:        config.Config{},
-			ExcludeAfter:  time.Now().Add(1 * time.Hour * -1),
-			FirstSeenTime: time.Now(),
-			Expected:      false,
+		"timeAfterExclusionFilter": {
+			configObj: config.ResourceType{
+				ExcludeRule: config.FilterRule{
+					TimeAfter: aws.Time(now.Add(-1 * time.Hour)),
+				}},
+			expected: []string{},
 		},
 	}
-
-	for _, c := range cases {
-		t.Run(c.Name, func(t *testing.T) {
-			result := shouldIncludeAllocationId(c.Address, c.ExcludeAfter, c.FirstSeenTime, c.Config)
-			assert.Equal(t, c.Expected, result)
+	for name, tc := range tests {
+		t.Run(name, func(t *testing.T) {
+			names, err := ea.getAll(config.Config{
+				ElasticIP: tc.configObj,
+			})
+			require.NoError(t, err)
+			require.Equal(t, tc.expected, awsgo.StringValueSlice(names))
 		})
 	}
+
+}
+
+func TestEIPAddress_NukeAll(t *testing.T) {
+	telemetry.InitTelemetry("cloud-nuke", "")
+	t.Parallel()
+
+	ea := EIPAddresses{
+		Client: &mockedEIPAddresses{
+			ReleaseAddressOutput: ec2.ReleaseAddressOutput{},
+		},
+	}
+
+	err := ea.nukeAll([]*string{awsgo.String("alloc1")})
+	require.NoError(t, err)
 }
