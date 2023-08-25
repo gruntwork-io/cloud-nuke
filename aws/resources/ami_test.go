0d4b8097055ee678b7f5eb69a93213196e5fe257
James Kwon <96548424+hongil0316@users.noreply.github.com>
2023-08-25 17:50:45 -0400

    Skip AWS Managed AMIs (#569)

--- aws/resources/ami_test.go
+++ aws/resources/ami_test.go
@@ -1,6 +1,7 @@
 package resources
 
 import (
+	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go/service/ec2/ec2iface"
 	"github.com/gruntwork-io/cloud-nuke/config"
 	"github.com/gruntwork-io/cloud-nuke/telemetry"
@@ -25,6 +26,51 @@ func (m mockedAMI) DescribeImages(input *ec2.DescribeImagesInput) (*ec2.Describe
 
 func (m mockedAMI) DeregisterImage(input *ec2.DeregisterImageInput) (*ec2.DeregisterImageOutput, error) {
 	return &m.DeregisterImageOutput, nil
+}
+
+func TestAMIGetAll_SkipAWSManaged(t *testing.T) {
+	telemetry.InitTelemetry("cloud-nuke", "")
+	t.Parallel()
+
+	testName := "test-ami"
+	testImageId1 := "test-image-id1"
+	testImageId2 := "test-image-id2"
+	now := time.Now()
+	acm := AMIs{
+		Client: mockedAMI{
+			DescribeImagesOutput: ec2.DescribeImagesOutput{
+				Images: []*ec2.Image{
+					{
+						ImageId:      &testImageId1,
+						Name:         &testName,
+						CreationDate: awsgo.String(now.Format("2006-01-02T15:04:05.000Z")),
+						Tags: []*ec2.Tag{
+							{
+								Key:   aws.String("aws-managed"),
+								Value: aws.String("true"),
+							},
+						},
+					},
+					{
+						ImageId:      &testImageId2,
+						Name:         aws.String("AwsBackup_Test"),
+						CreationDate: awsgo.String(now.Format("2006-01-02T15:04:05.000Z")),
+						Tags: []*ec2.Tag{
+							{
+								Key:   aws.String("aws-managed"),
+								Value: aws.String("true"),
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+
+	amis, err := acm.getAll(config.Config{})
+	assert.NoError(t, err)
+	assert.NotContains(t, awsgo.StringValueSlice(amis), testImageId1)
+	assert.NotContains(t, awsgo.StringValueSlice(amis), testImageId2)
 }
 
 func TestAMIGetAll(t *testing.T) {
