78077aa061efee0026e674ea27bacca405e2f1f5
James Kwon <james@gruntwork.io>
2023-09-07 20:11:23 -0400

    address test failures

--- aws/types_test.go
+++ aws/types_test.go
@@ -1,6 +1,7 @@
 package aws
 
 import (
+	"github.com/aws/aws-sdk-go/aws"
 	"testing"
 	"time"
 
@@ -17,7 +18,8 @@ func TestNewQueryAcceptsValidExcludeAfterEntries(t *testing.T) {
 		ExcludeRegions       []string
 		ResourceTypes        []string
 		ExcludeResourceTypes []string
-		ExcludeAfter         time.Time
+		ExcludeAfter         *time.Time
+		IncludeAfter         *time.Time
 	}
 
 	testCases := []TestCase{
@@ -26,20 +28,22 @@ func TestNewQueryAcceptsValidExcludeAfterEntries(t *testing.T) {
 			Regions:        []string{"us-east-1"},
 			ExcludeRegions: []string{},
 			ResourceTypes:  []string{"ec2"},
-			ExcludeAfter:   time.Now().Add(time.Hour * 24),
+			ExcludeAfter:   aws.Time(time.Now().Add(time.Hour * 24)),
+			IncludeAfter:   aws.Time(time.Now().Add(time.Hour * 24)),
 		},
 		{
 			Name:           "Can pass time.Now",
 			Regions:        []string{"us-east-1"},
 			ExcludeRegions: []string{},
 			ResourceTypes:  []string{"ec2"},
-			ExcludeAfter:   time.Now(),
+			ExcludeAfter:   aws.Time(time.Now()),
+			IncludeAfter:   aws.Time(time.Now()),
 		},
 	}
 
 	for _, tc := range testCases {
 		t.Run(tc.Name, func(t *testing.T) {
-			_, err := NewQuery(tc.Regions, tc.ExcludeRegions, tc.ResourceTypes, tc.ExcludeResourceTypes, tc.ExcludeAfter, false)
+			_, err := NewQuery(tc.Regions, tc.ExcludeRegions, tc.ResourceTypes, tc.ExcludeResourceTypes, tc.ExcludeAfter, tc.IncludeAfter, false)
 			require.NoError(t, err)
 		})
 	}
