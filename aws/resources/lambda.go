0df9701f34bb52c5a4fc9f355986df99a5b72b30
Derek Liang <fr.derekliang@gmail.com>
2023-10-12 09:03:18 -0700

    update readme for running tests in aws/resources

--- aws/resources/lambda.go
+++ aws/resources/lambda.go
@@ -2,16 +2,12 @@ package resources
 
 import (
 	"context"
-	"github.com/gruntwork-io/cloud-nuke/telemetry"
-	commonTelemetry "github.com/gruntwork-io/go-commons/telemetry"
 	"time"
 
 	"github.com/aws/aws-sdk-go/aws"
-	awsgo "github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/lambda"
 	"github.com/gruntwork-io/cloud-nuke/config"
 	"github.com/gruntwork-io/cloud-nuke/logging"
-	"github.com/gruntwork-io/cloud-nuke/report"
 )
 
 func (lf *LambdaFunctions) getAll(c context.Context, configObj config.Config) ([]*string, error) {
@@ -64,33 +60,33 @@ func (lf *LambdaFunctions) nukeAll(names []*string) error {
 	logging.Logger.Debugf("Deleting all Lambda Functions in region %s", lf.Region)
 	deletedNames := []*string{}
 
-	for _, name := range names {
-		params := &lambda.DeleteFunctionInput{
-			FunctionName: name,
-		}
+	// for _, name := range names {
+	// 	params := &lambda.DeleteFunctionInput{
+	// 		FunctionName: name,
+	// 	}
 
-		_, err := lf.Client.DeleteFunction(params)
+	// 	_, err := lf.Client.DeleteFunction(params)
 
-		// Record status of this resource
-		e := report.Entry{
-			Identifier:   aws.StringValue(name),
-			ResourceType: "Lambda function",
-			Error:        err,
-		}
-		report.Record(e)
+	// 	// Record status of this resource
+	// 	e := report.Entry{
+	// 		Identifier:   aws.StringValue(name),
+	// 		ResourceType: "Lambda function",
+	// 		Error:        err,
+	// 	}
+	// 	report.Record(e)
 
-		if err != nil {
-			logging.Logger.Errorf("[Failed] %s: %s", *name, err)
-			telemetry.TrackEvent(commonTelemetry.EventContext{
-				EventName: "Error Nuking Lambda Function",
-			}, map[string]interface{}{
-				"region": lf.Region,
-			})
-		} else {
-			deletedNames = append(deletedNames, name)
-			logging.Logger.Debugf("Deleted Lambda Function: %s", awsgo.StringValue(name))
-		}
-	}
+	// 	if err != nil {
+	// 		logging.Logger.Errorf("[Failed] %s: %s", *name, err)
+	// 		telemetry.TrackEvent(commonTelemetry.EventContext{
+	// 			EventName: "Error Nuking Lambda Function",
+	// 		}, map[string]interface{}{
+	// 			"region": lf.Region,
+	// 		})
+	// 	} else {
+	// 		deletedNames = append(deletedNames, name)
+	// 		logging.Logger.Debugf("Deleted Lambda Function: %s", awsgo.StringValue(name))
+	// 	}
+	// }
 
 	logging.Logger.Debugf("[OK] %d Lambda Function(s) deleted in %s", len(deletedNames), lf.Region)
 	return nil
