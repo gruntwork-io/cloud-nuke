140b6d9dc58aed6a712e1cafe9c8c47ebaba6a8c
James Kwon <96548424+hongil0316@users.noreply.github.com>
2024-02-19 05:50:17 -0500

    address comments

--- aws/resources/ses_configuration_set.go
+++ aws/resources/ses_configuration_set.go
@@ -11,9 +11,9 @@ import (
 	"github.com/gruntwork-io/go-commons/errors"
 )
 
-// Returns a formatted string of ses-configuartion set names
+// Returns a formatted string of ses-configuration set names
 func (s *SesConfigurationSet) getAll(c context.Context, configObj config.Config) ([]*string, error) {
-	// Remove defalt route table, that will be deleted along with its TransitGateway
+	// Remove default route table, that will be deleted along with its TransitGateway
 	param := &ses.ListConfigurationSetsInput{}
 
 	result, err := s.Client.ListConfigurationSets(param)
@@ -34,11 +34,11 @@ func (s *SesConfigurationSet) getAll(c context.Context, configObj config.Config)
 // Deletes all sets
 func (s *SesConfigurationSet) nukeAll(sets []*string) error {
 	if len(sets) == 0 {
-		logging.Debugf("No SES configuartion sets to nuke in region %s", s.Region)
+		logging.Debugf("No SES configuration sets to nuke in region %s", s.Region)
 		return nil
 	}
 
-	logging.Debugf("Deleting all SES configuartion sets in region %s", s.Region)
+	logging.Debugf("Deleting all SES configuration sets in region %s", s.Region)
 	var deletedSets []*string
 
 	for _, set := range sets {
@@ -49,7 +49,7 @@ func (s *SesConfigurationSet) nukeAll(sets []*string) error {
 		// Record status of this resource
 		e := report.Entry{
 			Identifier:   aws.StringValue(set),
-			ResourceType: "SES configuartion set",
+			ResourceType: "SES configuration set",
 			Error:        err,
 		}
 		report.Record(e)
@@ -58,11 +58,11 @@ func (s *SesConfigurationSet) nukeAll(sets []*string) error {
 			logging.Debugf("[Failed] %s", err)
 		} else {
 			deletedSets = append(deletedSets, set)
-			logging.Debugf("Deleted SES configuartion set: %s", *set)
+			logging.Debugf("Deleted SES configuration set: %s", *set)
 		}
 	}
 
-	logging.Debugf("[OK] %d SES configuartion set(s) deleted in %s", len(deletedSets), s.Region)
+	logging.Debugf("[OK] %d SES configuration set(s) deleted in %s", len(deletedSets), s.Region)
 
 	return nil
 }
