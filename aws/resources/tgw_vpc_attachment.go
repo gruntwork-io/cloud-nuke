bcff733dd9396b51cc900aacb815d6f23fbab024
James Kwon <96548424+hongil0316@users.noreply.github.com>
2024-10-25 21:31:51 -0400

    migrate: aws sdk v2 for transit-gateway-attachment

--- aws/resources/tgw_vpc_attachment.go
+++ aws/resources/tgw_vpc_attachment.go
@@ -4,9 +4,9 @@ import (
 	"context"
 	"time"
 
-	"github.com/aws/aws-sdk-go/aws"
-	awsgo "github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/service/ec2"
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/service/ec2"
+	"github.com/aws/aws-sdk-go-v2/service/ec2/types"
 	goerror "github.com/go-errors/errors"
 	"github.com/gruntwork-io/cloud-nuke/config"
 	"github.com/gruntwork-io/cloud-nuke/logging"
@@ -14,22 +14,32 @@ import (
 	"github.com/gruntwork-io/go-commons/errors"
 )
 
-// Returns a formated string of TransitGatewayVpcAttachment IDs
-func (tgw *TransitGatewaysVpcAttachment) getAll(c context.Context, configObj config.Config) ([]*string, error) {
-	result, err := tgw.Client.DescribeTransitGatewayVpcAttachmentsWithContext(tgw.Context, &ec2.DescribeTransitGatewayVpcAttachmentsInput{})
-	if err != nil {
-		return nil, errors.WithStackTrace(err)
-	}
+// Returns a formatted string of TransitGatewayVpcAttachment IDs
+func (tgw *TransitGatewaysVpcAttachment) getAll(ctx context.Context, configObj config.Config) ([]*string, error) {
+	var identifiers []*string
+	params := &ec2.DescribeTransitGatewayVpcAttachmentsInput{}
 
-	var ids []*string
-	for _, tgwVpcAttachment := range result.TransitGatewayVpcAttachments {
-		if configObj.TransitGatewaysVpcAttachment.ShouldInclude(config.ResourceValue{Time: tgwVpcAttachment.CreationTime}) &&
-			awsgo.StringValue(tgwVpcAttachment.State) != "deleted" && awsgo.StringValue(tgwVpcAttachment.State) != "deleting" {
-			ids = append(ids, tgwVpcAttachment.TransitGatewayAttachmentId)
+	hasMorePages := true
+	for hasMorePages {
+		result, err := tgw.Client.DescribeTransitGatewayVpcAttachments(ctx, params)
+		if err != nil {
+			logging.Debugf("[Transit Gateway] Failed to list transit gateway VPC attachments: %s", err)
+			return nil, errors.WithStackTrace(err)
 		}
+
+		for _, tgwVpcAttachment := range result.TransitGatewayVpcAttachments {
+			if configObj.TransitGatewaysVpcAttachment.ShouldInclude(config.ResourceValue{
+				Time: tgwVpcAttachment.CreationTime,
+			}) && tgwVpcAttachment.State != "deleted" && tgwVpcAttachment.State != "deleting" {
+				identifiers = append(identifiers, tgwVpcAttachment.TransitGatewayAttachmentId)
+			}
+		}
+
+		params.NextToken = result.NextToken
+		hasMorePages = params.NextToken != nil
 	}
 
-	return ids, nil
+	return identifiers, nil
 }
 
 // Delete all TransitGatewayVpcAttachments
@@ -47,12 +57,12 @@ func (tgw *TransitGatewaysVpcAttachment) nukeAll(ids []*string) error {
 			TransitGatewayAttachmentId: id,
 		}
 
-		_, err := tgw.Client.DeleteTransitGatewayVpcAttachmentWithContext(tgw.Context, param)
+		_, err := tgw.Client.DeleteTransitGatewayVpcAttachment(tgw.Context, param)
 
 		// Record status of this resource
 		e := report.Entry{
-			Identifier:   aws.StringValue(id),
-			ResourceType: "Transit Gateway",
+			Identifier:   aws.ToString(id),
+			ResourceType: tgw.ResourceName(),
 			Error:        err,
 		}
 		report.Record(e)
@@ -65,22 +75,22 @@ func (tgw *TransitGatewaysVpcAttachment) nukeAll(ids []*string) error {
 		}
 	}
 
-	if waiterr := waitForTransitGatewayAttachementToBeDeleted(*tgw); waiterr != nil {
+	if waiterr := waitForTransitGatewayAttachmentToBeDeleted(*tgw); waiterr != nil {
 		return errors.WithStackTrace(waiterr)
 	}
 	logging.Debugf("[OK] %d Transit Gateway Vpc Attachment(s) deleted in %s", len(deletedIds), tgw.Region)
 	return nil
 }
 
-func waitForTransitGatewayAttachementToBeDeleted(tgw TransitGatewaysVpcAttachment) error {
+func waitForTransitGatewayAttachmentToBeDeleted(tgw TransitGatewaysVpcAttachment) error {
 	for i := 0; i < 30; i++ {
 		gateways, err := tgw.Client.DescribeTransitGatewayVpcAttachments(
-			&ec2.DescribeTransitGatewayVpcAttachmentsInput{
-				TransitGatewayAttachmentIds: aws.StringSlice(tgw.Ids),
-				Filters: []*ec2.Filter{
+			tgw.Context, &ec2.DescribeTransitGatewayVpcAttachmentsInput{
+				TransitGatewayAttachmentIds: tgw.Ids,
+				Filters: []types.Filter{
 					{
-						Name:   awsgo.String("state"),
-						Values: []*string{awsgo.String("deleting")},
+						Name:   aws.String("state"),
+						Values: []string{"deleting"},
 					},
 				},
 			},
@@ -91,9 +101,9 @@ func waitForTransitGatewayAttachementToBeDeleted(tgw TransitGatewaysVpcAttachmen
 		if len(gateways.TransitGatewayVpcAttachments) == 0 {
 			return nil
 		}
-		logging.Info("Waiting for transit gateways attachemensts to be deleted...")
+		logging.Info("Waiting for transit gateways attachments to be deleted...")
 		time.Sleep(10 * time.Second)
 	}
 
-	return goerror.New("timed out waiting for transit gateway attahcments to be successfully deleted")
+	return goerror.New("timed out waiting for transit gateway attachments to be successfully deleted")
 }
--- go.mod
+++ go.mod
@@ -4,31 +4,31 @@ go 1.21
 
 require (
 	github.com/aws/aws-sdk-go v1.49.13
-	github.com/aws/aws-sdk-go-v2 v1.32.2
+	github.com/aws/aws-sdk-go-v2 v1.32.3
 	github.com/aws/aws-sdk-go-v2/config v1.27.24
 	github.com/aws/aws-sdk-go-v2/credentials v1.17.24
-	github.com/aws/aws-sdk-go-v2/service/accessanalyzer v1.34.2
-	github.com/aws/aws-sdk-go-v2/service/acm v1.30.2
-	github.com/aws/aws-sdk-go-v2/service/acmpca v1.37.3
+	github.com/aws/aws-sdk-go-v2/service/accessanalyzer v1.34.3
+	github.com/aws/aws-sdk-go-v2/service/acm v1.30.3
+	github.com/aws/aws-sdk-go-v2/service/acmpca v1.37.4
 	github.com/aws/aws-sdk-go-v2/service/amp v1.27.1
-	github.com/aws/aws-sdk-go-v2/service/apigateway v1.27.2
-	github.com/aws/aws-sdk-go-v2/service/apigatewayv2 v1.24.2
-	github.com/aws/aws-sdk-go-v2/service/apprunner v1.32.2
+	github.com/aws/aws-sdk-go-v2/service/apigateway v1.27.3
+	github.com/aws/aws-sdk-go-v2/service/apigatewayv2 v1.24.3
+	github.com/aws/aws-sdk-go-v2/service/apprunner v1.32.3
 	github.com/aws/aws-sdk-go-v2/service/autoscaling v1.23.16
-	github.com/aws/aws-sdk-go-v2/service/backup v1.39.3
-	github.com/aws/aws-sdk-go-v2/service/cloudtrail v1.44.2
-	github.com/aws/aws-sdk-go-v2/service/cloudwatch v1.42.2
-	github.com/aws/aws-sdk-go-v2/service/cloudwatchlogs v1.42.0
-	github.com/aws/aws-sdk-go-v2/service/codedeploy v1.29.2
-	github.com/aws/aws-sdk-go-v2/service/configservice v1.50.2
-	github.com/aws/aws-sdk-go-v2/service/datasync v1.42.2
-	github.com/aws/aws-sdk-go-v2/service/dynamodb v1.36.2
-	github.com/aws/aws-sdk-go-v2/service/ec2 v1.184.0
+	github.com/aws/aws-sdk-go-v2/service/backup v1.39.4
+	github.com/aws/aws-sdk-go-v2/service/cloudtrail v1.44.3
+	github.com/aws/aws-sdk-go-v2/service/cloudwatch v1.42.3
+	github.com/aws/aws-sdk-go-v2/service/cloudwatchlogs v1.43.0
+	github.com/aws/aws-sdk-go-v2/service/codedeploy v1.29.3
+	github.com/aws/aws-sdk-go-v2/service/configservice v1.50.3
+	github.com/aws/aws-sdk-go-v2/service/datasync v1.43.0
+	github.com/aws/aws-sdk-go-v2/service/dynamodb v1.36.3
+	github.com/aws/aws-sdk-go-v2/service/ec2 v1.185.0
 	github.com/aws/aws-sdk-go-v2/service/eventbridge v1.33.7
 	github.com/aws/aws-sdk-go-v2/service/scheduler v1.10.7
 	github.com/aws/aws-sdk-go-v2/service/sns v1.32.3
 	github.com/aws/aws-sdk-go-v2/service/sqs v1.35.3
-	github.com/aws/aws-sdk-go-v2/service/vpclattice v1.12.2
+	github.com/aws/aws-sdk-go-v2/service/vpclattice v1.12.3
 	github.com/charmbracelet/lipgloss v0.6.0
 	github.com/go-errors/errors v1.4.2
 	github.com/gruntwork-io/go-commons v0.17.0
@@ -46,12 +46,12 @@ require (
 	atomicgo.dev/keyboard v0.2.8 // indirect
 	github.com/aws/aws-sdk-go-v2/aws/protocol/eventstream v1.6.6 // indirect
 	github.com/aws/aws-sdk-go-v2/feature/ec2/imds v1.16.9 // indirect
-	github.com/aws/aws-sdk-go-v2/internal/configsources v1.3.21 // indirect
-	github.com/aws/aws-sdk-go-v2/internal/endpoints/v2 v2.6.21 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/configsources v1.3.22 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/endpoints/v2 v2.6.22 // indirect
 	github.com/aws/aws-sdk-go-v2/internal/ini v1.8.0 // indirect
 	github.com/aws/aws-sdk-go-v2/internal/v4a v1.3.17 // indirect
 	github.com/aws/aws-sdk-go-v2/service/internal/accept-encoding v1.12.0 // indirect
-	github.com/aws/aws-sdk-go-v2/service/internal/endpoint-discovery v1.10.2 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/endpoint-discovery v1.10.3 // indirect
 	github.com/aws/aws-sdk-go-v2/service/internal/presigned-url v1.12.2 // indirect
 	github.com/aws/aws-sdk-go-v2/service/sso v1.22.1 // indirect
 	github.com/aws/aws-sdk-go-v2/service/ssooidc v1.26.2 // indirect
