6d5181ef810f34b9447f1f9be0ac5a0f69d44afc
James Kwon <96548424+hongil0316@users.noreply.github.com>
2025-03-18 21:00:34 -0400

    disable deletion protection

--- aws/resources/rds_test.go
+++ aws/resources/rds_test.go
@@ -10,13 +10,24 @@ import (
 	"github.com/aws/aws-sdk-go-v2/service/rds"
 	"github.com/aws/aws-sdk-go-v2/service/rds/types"
 	"github.com/gruntwork-io/cloud-nuke/config"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
 type mockedDBInstance struct {
+	t *testing.T
+
 	RDSAPI
 	DescribeDBInstancesOutput rds.DescribeDBInstancesOutput
 	DeleteDBInstanceOutput    rds.DeleteDBInstanceOutput
+}
+
+func (m mockedDBInstance) ModifyDBInstance(ctx context.Context, params *rds.ModifyDBInstanceInput, optFns ...func(*rds.Options)) (*rds.ModifyDBInstanceOutput, error) {
+	assert.NotNil(m.t, params)
+	assert.NotEmpty(m.t, *params.DBInstanceIdentifier)
+	assert.False(m.t, *params.DeletionProtection)
+	assert.True(m.t, *params.ApplyImmediately)
+	return nil, nil
 }
 
 func (m mockedDBInstance) DescribeDBInstances(ctx context.Context, params *rds.DescribeDBInstancesInput, optFns ...func(*rds.Options)) (*rds.DescribeDBInstancesOutput, error) {
@@ -26,20 +37,25 @@ func (m mockedDBInstance) DescribeDBInstances(ctx context.Context, params *rds.D
 func (m mockedDBInstance) DeleteDBInstance(ctx context.Context, params *rds.DeleteDBInstanceInput, optFns ...func(*rds.Options)) (*rds.DeleteDBInstanceOutput, error) {
 	return &m.DeleteDBInstanceOutput, nil
 }
+
 func (m mockedDBInstance) WaitForOutput(ctx context.Context, params *rds.DescribeDBInstancesInput, maxWaitDur time.Duration, optFns ...func(*rds.Options)) (*rds.DescribeDBInstancesOutput, error) {
 	return nil, nil
 }
+
 func TestDBInstances_GetAll(t *testing.T) {
 
 	t.Parallel()
 
 	testName1 := "test-db-instance1"
 	testName2 := "test-db-instance2"
+	testName3 := "test-db-instance3"
 	testIdentifier1 := "test-identifier1"
 	testIdentifier2 := "test-identifier2"
+	testIdentifier3 := "test-identifier3"
 	now := time.Now()
 	di := DBInstances{
 		Client: mockedDBInstance{
+			t: t,
 			DescribeDBInstancesOutput: rds.DescribeDBInstancesOutput{
 				DBInstances: []types.DBInstance{
 					{
@@ -52,35 +68,49 @@ func TestDBInstances_GetAll(t *testing.T) {
 						DBName:               aws.String(testName2),
 						InstanceCreateTime:   aws.Time(now.Add(1)),
 					},
+					{
+						DBInstanceIdentifier: aws.String(testIdentifier3),
+						DBName:               aws.String(testName3),
+						InstanceCreateTime:   aws.Time(now.Add(1)),
+						DeletionProtection:   aws.Bool(true),
+					},
 				},
 			},
 		},
 	}
 
 	tests := map[string]struct {
-		configObj config.ResourceType
+		configObj config.AWSProtectectableResourceType
 		expected  []string
 	}{
 		"emptyFilter": {
-			configObj: config.ResourceType{},
+			configObj: config.AWSProtectectableResourceType{},
 			expected:  []string{testIdentifier1, testIdentifier2},
 		},
 		"nameExclusionFilter": {
-			configObj: config.ResourceType{
+			configObj: config.AWSProtectectableResourceType{ResourceType: config.ResourceType{
 				ExcludeRule: config.FilterRule{
 					NamesRegExp: []config.Expression{{
 						RE: *regexp.MustCompile("^" + testName1 + "$"),
 					}},
 				},
-			},
+			}},
 			expected: []string{testIdentifier1, testIdentifier2},
 		},
 		"timeAfterExclusionFilter": {
-			configObj: config.ResourceType{
-				ExcludeRule: config.FilterRule{
-					TimeAfter: aws.Time(now),
-				}},
+			configObj: config.AWSProtectectableResourceType{
+				ResourceType: config.ResourceType{
+					ExcludeRule: config.FilterRule{
+						TimeAfter: aws.Time(now),
+					}}},
 			expected: []string{testIdentifier1},
+		},
+		"includeDeletionProtection": {
+			configObj: config.AWSProtectectableResourceType{
+				IncludeDeletionProtected: true,
+				ResourceType:             config.ResourceType{},
+			},
+			expected: []string{testIdentifier1, testIdentifier2, testIdentifier3},
 		},
 	}
 	for name, tc := range tests {
@@ -100,6 +130,7 @@ func TestDBInstances_NukeAll(t *testing.T) {
 
 	di := DBInstances{
 		Client: mockedDBInstance{
+			t:                      t,
 			DeleteDBInstanceOutput: rds.DeleteDBInstanceOutput{},
 		},
 	}
