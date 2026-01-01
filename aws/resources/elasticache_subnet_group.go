f03091699901fb5f36f5922b76733064e56a5f77
james00012 <hongilkwon316@gmail.com>
2026-01-01 12:07:27 -0500

    refactor: split elasticache into separate files and migrate parameter/subnet groups

---
+++ aws/resources/elasticache_subnet_group.go
@@ -0,0 +1,73 @@
+package resources
+
+import (
+	"context"
+	"strings"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/service/elasticache"
+	"github.com/gruntwork-io/cloud-nuke/config"
+	"github.com/gruntwork-io/cloud-nuke/logging"
+	"github.com/gruntwork-io/cloud-nuke/resource"
+	"github.com/gruntwork-io/go-commons/errors"
+)
+
+// ElasticacheSubnetGroupsAPI defines the interface for Elasticache Subnet Group operations.
+type ElasticacheSubnetGroupsAPI interface {
+	DescribeCacheSubnetGroups(ctx context.Context, params *elasticache.DescribeCacheSubnetGroupsInput, optFns ...func(*elasticache.Options)) (*elasticache.DescribeCacheSubnetGroupsOutput, error)
+	DeleteCacheSubnetGroup(ctx context.Context, params *elasticache.DeleteCacheSubnetGroupInput, optFns ...func(*elasticache.Options)) (*elasticache.DeleteCacheSubnetGroupOutput, error)
+}
+
+// NewElasticacheSubnetGroups creates a new Elasticache Subnet Groups resource using the generic resource pattern.
+func NewElasticacheSubnetGroups() AwsResource {
+	return NewAwsResource(&resource.Resource[ElasticacheSubnetGroupsAPI]{
+		ResourceTypeName: "elasticacheSubnetGroups",
+		BatchSize:        49,
+		InitClient: func(r *resource.Resource[ElasticacheSubnetGroupsAPI], cfg any) {
+			awsCfg, ok := cfg.(aws.Config)
+			if !ok {
+				logging.Debugf("Invalid config type for Elasticache client: expected aws.Config")
+				return
+			}
+			r.Scope.Region = awsCfg.Region
+			r.Client = elasticache.NewFromConfig(awsCfg)
+		},
+		ConfigGetter: func(c config.Config) config.ResourceType {
+			return c.ElasticacheSubnetGroups
+		},
+		Lister: listElasticacheSubnetGroups,
+		Nuker:  resource.SimpleBatchDeleter(deleteElasticacheSubnetGroup),
+	})
+}
+
+// listElasticacheSubnetGroups retrieves all Elasticache subnet groups that match the config filters.
+func listElasticacheSubnetGroups(ctx context.Context, client ElasticacheSubnetGroupsAPI, scope resource.Scope, cfg config.ResourceType) ([]*string, error) {
+	var subnetGroupNames []*string
+
+	paginator := elasticache.NewDescribeCacheSubnetGroupsPaginator(client, &elasticache.DescribeCacheSubnetGroupsInput{})
+	for paginator.HasMorePages() {
+		page, err := paginator.NextPage(ctx)
+		if err != nil {
+			return nil, errors.WithStackTrace(err)
+		}
+
+		for _, subnetGroup := range page.CacheSubnetGroups {
+			if !strings.Contains(aws.ToString(subnetGroup.CacheSubnetGroupName), "default") &&
+				cfg.ShouldInclude(config.ResourceValue{
+					Name: subnetGroup.CacheSubnetGroupName,
+				}) {
+				subnetGroupNames = append(subnetGroupNames, subnetGroup.CacheSubnetGroupName)
+			}
+		}
+	}
+
+	return subnetGroupNames, nil
+}
+
+// deleteElasticacheSubnetGroup deletes a single Elasticache subnet group.
+func deleteElasticacheSubnetGroup(ctx context.Context, client ElasticacheSubnetGroupsAPI, identifier *string) error {
+	_, err := client.DeleteCacheSubnetGroup(ctx, &elasticache.DeleteCacheSubnetGroupInput{
+		CacheSubnetGroupName: identifier,
+	})
+	return err
+}
